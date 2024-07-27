7c965414a6d78a9948fd0c79bb6af982e4a9119d
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2024-07-27 17:28:12 +0200

    Some solutions

---
+++ Drafts/BinaryTreeDraft.cs
@@ -0,0 +1,87 @@
+﻿namespace Drafts
+{
+  public class BinaryTree
+  {
+    private class Node
+    {
+      private Node Left { get; set; } = null;
+      private Node Right { get; set; } = null;
+      private string Value { get; }
+
+      private int CountLeft => Left?.Count ?? 0;
+      private int CountRight => Right?.Count ?? 0;
+      private int Count => CountLeft + CountRight + 1;
+
+      public Node(string value) => Value = value;
+
+      public void Insert(string word)
+      {
+        var c = Value.CompareTo(word);
+        if (c < 0)
+        {
+          if (Left == null)
+            Left = new Node(word);
+          else
+            Left.Insert(word);
+        }
+        else if (c > 0)
+        {
+          if (Right == null)
+            Right = new Node(word);
+          else
+            Right.Insert(word);
+        }
+
+        if (Math.Abs(CountRight - CountLeft) > 1)
+        {
+          //Rebalance
+        }
+      }
+
+      public bool Search(string word)
+      {
+        var c = Value.CompareTo(word);
+        return c > 0
+          ? Right?.Search(word) ?? false
+          : c == 0 || (Left?.Search(word) ?? false);
+      }
+
+      public bool StartsWith(string prefix)
+      {
+        var length = Math.Min(prefix.Length, Value.Length);
+
+        for (var i = 0; i < length; i++)
+        {
+          var c = Value[i].CompareTo(prefix[i]);
+
+          if (c > 0)
+            return Right?.StartsWith(prefix) ?? false;
+          else if (c < 0)
+          {
+            return Left?.StartsWith(prefix) ?? false;
+          }
+        }
+
+        return Value.StartsWith(prefix);
+      }
+    }
+
+    private Node Root { get; set; } = null;
+
+    public void Insert(string word)
+    {
+      if (Root == null)
+        Root = new Node(word);
+      else
+      {
+        Root.Insert(word);
+      }
+    }
+
+    public bool Search(string word)
+      => Root != null && Root.Search(word);
+
+    public bool StartsWith(string prefix)
+      => Root != null && Root.StartsWith(prefix);
+  }
+}
---
+++ Problems/2976. Minimum Cost to Convert String I.cs
@@ -0,0 +1,68 @@
+﻿namespace Problem2976
+{
+  /// <summary>
+  /// 2976. Minimum Cost to Convert String I
+  /// https://leetcode.com/problems/minimum-cost-to-convert-string-i/?envType=daily-question&envId=2024-07-27
+  /// Difficulty Medium 54.6%
+  /// 
+  /// Floyd Warshall Algorithm
+  /// Graph
+  /// Shortest Path
+  /// </summary>
+  public class Solution
+  {
+    private const int N = 26;
+    private const int NoWay = -1;
+
+    public long MinimumCost(string source, string target, char[] original, char[] changed, int[] cost)
+    {
+      var costs = new long[N, N];
+      for (var k = 0; k < N; k++)
+      {
+        for (var i = 0; i < N; i++)
+          costs[k, i] = NoWay;
+      }
+
+      for (var k = 0; k < N; k++)
+        costs[k, k] = 0;
+
+      var edgesCount = cost.Length;
+      for (var i = 0; i < edgesCount; i++)
+      {
+        var from = original[i] - 'a';
+        var to = changed[i] - 'a';
+        if (costs[from, to] < 0 || costs[from, to] > cost[i])
+          costs[from, to] = cost[i];
+      }
+
+      //Floyd Warshall Algorithm
+      for (var k = 0; k < N; k++)
+      {
+        for (var i = 0; i < N; i++)
+        {
+          for (var j = 0; j < N; j++)
+          {
+            if (costs[i, k] > 0 && costs[k, j] > 0 &&
+              (costs[i, j] > costs[i, k] + costs[k, j] || costs[i, j] < 0))
+              costs[i, j] = costs[i, k] + costs[k, j];
+          }
+        }
+      }
+
+      var result = 0l;
+      var charCount = source.Length;
+      for (var i = 0; i < charCount; i++)
+      {
+        var from = source[i] - 'a';
+        var to = target[i] - 'a';
+        if (costs[from, to] >= 0)
+          result += costs[from, to];
+        else
+        {
+          return -1;
+        }
+      }
+      return result;
+    }
+  }
+}
