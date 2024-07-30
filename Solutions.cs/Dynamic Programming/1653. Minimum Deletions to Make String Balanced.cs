f9a60baea980cb1802d2be65447bfb7ed3774d33
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2024-07-30 14:12:57 +0200

    1653. Minimum Deletions to Make String Balanced

---
+++ Solutions.cs/Dynamic Programming/1653. Minimum Deletions to Make String Balanced.cs
@@ -0,0 +1,36 @@
+﻿namespace Problem1653
+{
+
+  /// <summary>
+  /// 1653. Minimum Deletions to Make String Balanced
+  /// https://leetcode.com/problems/minimum-deletions-to-make-string-balanced
+  /// 
+  /// Difficulty Medium
+  /// Acceptance 63.0%
+  /// 
+  /// String
+  /// Dynamic Programming
+  /// Stack
+  /// </summary>
+  public class Solution
+  {
+    public int MinimumDeletions(string s)
+    {
+      var result = 0;
+      var bCount = 0;
+      for (var i = 0; i < s.Length; i++)
+      {
+        if (s[i] == 'b')
+        {
+          bCount++;
+        }
+        else
+        {
+          result = Math.Min(result + 1, bCount);
+        }
+      }
+
+      return result;
+    }
+  }
+}
