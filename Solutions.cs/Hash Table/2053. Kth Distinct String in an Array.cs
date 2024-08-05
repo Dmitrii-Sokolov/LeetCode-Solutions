e876001496f9e48177e5366fcaab6e4664e22521
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2024-08-05 09:49:44 +0200

    2053. Kth Distinct String in an Array

---
+++ Solutions.cs/Hash Table/2053. Kth Distinct String in an Array.cs
@@ -0,0 +1,36 @@
+﻿namespace Problem2053
+{
+
+  /// <summary>
+  /// 2053. Kth Distinct String in an Array
+  /// https://leetcode.com/problems/kth-distinct-string-in-an-array
+  /// 
+  /// Difficulty Easy
+  /// Acceptance 77.6%
+  /// 
+  /// Array
+  /// Hash Table
+  /// String
+  /// Counting
+  /// </summary>
+  public class Solution
+  {
+    public string KthDistinct(string[] arr, int k)
+    {
+      var dictionary = new Dictionary<string, int>();
+      foreach (var item in arr)
+        dictionary[item] = dictionary.GetValueOrDefault(item) + 1;
+
+      foreach (var item in arr)
+      {
+        if (dictionary[item] == 1)
+          k--;
+
+        if (k == 0)
+          return item;
+      }
+
+      return "";
+    }
+  }
+}
--- Solutions.cs/Prefix Sum/1508. Range Sum of Sorted Subarray Sums.cs
+++ Solutions.cs/Prefix Sum/1508. Range Sum of Sorted Subarray Sums.cs
@@ -1,54 +1,58 @@
-﻿/// <summary>
-/// 1508. Range Sum of Sorted Subarray Sums
-/// https://leetcode.com/problems/range-sum-of-sorted-subarray-sums
-/// 
-/// Difficulty Medium
-/// Acceptance 61.1%
-/// 
-/// Array
-/// Two Pointers
-/// Binary Search
-/// Sorting
-/// </summary>
-public class Solution
+﻿namespace Problem1508
 {
-  private const int Modulo = 1000000007;
 
-  public int RangeSum(int[] nums, int n, int left, int right)
+  /// <summary>
+  /// 1508. Range Sum of Sorted Subarray Sums
+  /// https://leetcode.com/problems/range-sum-of-sorted-subarray-sums
+  /// 
+  /// Difficulty Medium
+  /// Acceptance 61.1%
+  /// 
+  /// Array
+  /// Two Pointers
+  /// Binary Search
+  /// Sorting
+  /// </summary>
+  public class Solution
   {
-    var sums = new int[n * (n + 1) / 2];
-    var partialSums = new int[n];
-    var sum = 0;
-    for (var i = 0; i < n; i++)
+    private const int Modulo = 1000000007;
+
+    public int RangeSum(int[] nums, int n, int left, int right)
     {
-      sum += nums[i];
-      partialSums[i] = sum;
-    }
-
-    var start = 0;
-    var end = 0;
-    var p = 0;
-
-    while (end < n)
-    {
-      sums[p] = partialSums[end] - partialSums[start] + nums[start];
-      start++;
-
-      if (start > end)
+      var sums = new int[n * (n + 1) / 2];
+      var partialSums = new int[n];
+      var sum = 0;
+      for (var i = 0; i < n; i++)
       {
-        end++;
-        start = 0;
+        sum += nums[i];
+        partialSums[i] = sum;
       }
 
-      p++;
+      var start = 0;
+      var end = 0;
+      var p = 0;
+
+      while (end < n)
+      {
+        sums[p] = partialSums[end] - partialSums[start] + nums[start];
+        start++;
+
+        if (start > end)
+        {
+          end++;
+          start = 0;
+        }
+
+        p++;
+      }
+
+      Array.Sort(sums);
+
+      var result = 0;
+      for (var i = left; i <= right; i++)
+        result = (result % Modulo) + (sums[i - 1] % Modulo);
+
+      return result;
     }
-
-    Array.Sort(sums);
-
-    var result = 0;
-    for (var i = left; i <= right; i++)
-      result = (result % Modulo) + (sums[i - 1] % Modulo);
-
-    return result;
   }
 }
