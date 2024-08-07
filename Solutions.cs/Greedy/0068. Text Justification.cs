fbea31e3e22e1231c92d872e695345cbe507dae4
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2024-08-07 16:42:17 +0200

    Top Interview 150 (part)

---
+++ Solutions.cs/Greedy/0068. Text Justification.cs
@@ -0,0 +1,60 @@
+﻿namespace Problem68
+{
+
+  /// <summary>
+  /// 68. Text Justification
+  /// https://leetcode.com/problems/text-justification
+  /// 
+  /// Difficulty Hard
+  /// Acceptance 44.5%
+  /// 
+  /// Array
+  /// String
+  /// Simulation
+  /// </summary>
+  public class Solution
+  {
+    private const char Space = ' ';
+
+    public IList<string> FullJustify(string[] words, int maxWidth)
+    {
+      var line = new System.Text.StringBuilder();
+      var result = new List<string>();
+      var index = 0;
+
+      while (index < words.Length)
+      {
+        line.Clear();
+        var firstIndex = index;
+        var lineCharCount = 0;
+        while (index < words.Length && lineCharCount + words[index].Length <= maxWidth)
+          lineCharCount += words[index++].Length + 1;
+
+        var wordsCount = index - firstIndex;
+        var spacesCount = maxWidth - lineCharCount + wordsCount;
+        var gap = wordsCount == 1 ? spacesCount : spacesCount / (wordsCount - 1);
+        var remainder = wordsCount == 1 ? 0 : spacesCount % (wordsCount - 1);
+        var isFirst = true;
+
+        for (; firstIndex < index; firstIndex++)
+        {
+          if (!isFirst)
+          {
+            var spaces = index == words.Length ? 1 : gap + (remainder-- > 0 ? 1 : 0);
+            line.Append(Space, spaces);
+          }
+
+          line.Append(words[firstIndex]);
+          isFirst = false;
+        }
+
+        if (line.Length < maxWidth)
+          line.Append(Space, maxWidth - line.Length);
+
+        result.Add(line.ToString());
+      }
+
+      return result;
+    }
+  }
+}
---
+++ Solutions.cs/Hash Table/0380. Insert Delete GetRandom O(1).cs
@@ -0,0 +1,43 @@
+﻿namespace Problem380
+{
+
+  /// <summary>
+  /// 380. Insert Delete GetRandom O(1)
+  /// https://leetcode.com/problems/insert-delete-getrandom-o1
+  /// 
+  /// Difficulty Medium
+  /// Acceptance 54.6%
+  /// 
+  /// Array
+  /// Hash Table
+  /// Math
+  /// Design
+  /// Randomized
+  /// </summary>
+  public class RandomizedSet
+  {
+    private HashSet<int> ValuesSet = new();
+    private List<int> Values = new();
+    private Random Random = new();
+
+    public bool Insert(int value)
+    {
+      var added = ValuesSet.Add(value);
+      if (ValuesSet.Add(value))
+        Values.Add(value);
+
+      return added;
+    }
+
+    public bool Remove(int value)
+    {
+      var removed = ValuesSet.Remove(value);
+      if (removed)
+        Values.Remove(value);
+
+      return removed;
+    }
+
+    public int GetRandom() => Values[Random.Next(ValuesSet.Count)];
+  }
+}
