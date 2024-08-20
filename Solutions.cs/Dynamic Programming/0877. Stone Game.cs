dff978f897b792c76cfe16ad593a74147facfd3a
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2024-08-20 09:40:00 +0200

    1140. Stone Game II

---
+++ Solutions.cs/Dynamic Programming/0877. Stone Game.cs
@@ -0,0 +1,46 @@
+﻿namespace Problem877
+{
+
+  /// <summary>
+  /// 877. Stone Game
+  /// https://leetcode.com/problems/stone-game
+  /// 
+  /// Difficulty Medium
+  /// Acceptance 70.8%
+  /// 
+  /// Array
+  /// Math
+  /// Dynamic Programming
+  /// Game Theory
+  /// </summary>
+  public class Solution
+  {
+    private int[,] Results;
+
+    public bool StoneGame(int[] piles) => true;
+
+    public bool StoneGameCorrect(int[] piles)
+    {
+      Results = new int[piles.Length, piles.Length];
+      for (var i = 0; i < piles.Length; i++)
+        Results[i, i] = piles[i];
+
+      var sum = piles.Sum();
+      var aliceScore = Check(piles, 0, piles.Length - 1, sum);
+
+      return aliceScore > sum >> 1;
+    }
+
+    private int Check(int[] piles, int left, int right, int stones)
+    {
+      if (Results[left, right] == 0)
+      {
+        Results[left, right] = Math.Max(
+          stones - Check(piles, left + 1, right, stones - piles[left]),
+          stones - Check(piles, left, right - 1, stones - piles[right]));
+      }
+
+      return Results[left, right];
+    }
+  }
+}
---
+++ Solutions.cs/Dynamic Programming/1140. Stone Game II.cs
@@ -0,0 +1,53 @@
+﻿namespace Problem1140
+{
+
+  /// <summary>
+  /// 1140. Stone Game II
+  /// https://leetcode.com/problems/stone-game-ii
+  /// 
+  /// Difficulty Medium
+  /// Acceptance 69.4%
+  /// 
+  /// Array
+  /// Math
+  /// Dynamic Programming
+  /// Prefix Sum
+  /// Game Theory
+  /// </summary>
+  public class Solution
+  {
+    public int StoneGameII(int[] piles)
+      => Check([], piles, piles.Sum());
+
+    private int Check(Dictionary<State, int> results, int[] piles, int stones, int pointer = 0, int m = 1)
+    {
+      if (!results.TryGetValue(new State(pointer, m), out var result))
+      {
+        result = 0;
+        if (piles.Length - pointer <= 2 * m)
+        {
+          for (var i = pointer; i < piles.Length; i++)
+            result += piles[i];
+        }
+        else
+        {
+          var count = 0;
+          for (var i = 1; i <= 2 * m; i++)
+          {
+            count += piles[pointer + i - 1];
+            result = Math.Max(result, stones - Check(results, piles, stones - count, pointer + i, Math.Max(i, m)));
+          }
+        }
+
+        results.Add(new State(pointer, m), result);
+      }
+
+      return result;
+    }
+
+    private record struct State(int Pointer, int M) : IEquatable<State>
+    {
+      public override int GetHashCode() => HashCode.Combine(Pointer, M);
+    }
+  }
+}
