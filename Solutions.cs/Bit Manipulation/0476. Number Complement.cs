dbf50747a9405215e87537df0567e7c5fdec6f61
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2024-08-22 11:58:56 +0200

    0476. Number Complement

---
+++ Solutions.cs/Bit Manipulation/0476. Number Complement.cs
@@ -0,0 +1,28 @@
+﻿namespace Problem476
+{
+
+  /// <summary>
+  /// 476. Number Complement
+  /// https://leetcode.com/problems/number-complement
+  /// 
+  /// Difficulty Easy
+  /// Acceptance 68.6%
+  /// 
+  /// Bit Manipulation
+  /// </summary>
+  public class Solution
+  {
+    public int FindComplement(int num)
+    {
+      var result = 0;
+      var bit = 1L;
+      while (num >= bit)
+      {
+        result += ~num & (int)bit;
+        bit <<= 1;
+      }
+
+      return result;
+    }
+  }
+}
---
+++ Solutions.cs/Bit Manipulation/1009. Complement of Base 10 Integer.cs
@@ -0,0 +1,31 @@
+﻿namespace Problem1009
+{
+
+  /// <summary>
+  /// 1009. Complement of Base 10 Integer
+  /// https://leetcode.com/problems/complement-of-base-10-integer
+  /// 
+  /// Difficulty Easy
+  /// Acceptance 60.8%
+  /// 
+  /// Bit Manipulation
+  /// </summary>
+  public class Solution
+  {
+    public int BitwiseComplement(int num)
+    {
+      if (num == 0)
+        return 1;
+
+      var result = 0;
+      var bit = 1L;
+      while (num >= bit)
+      {
+        result += ~num & (int)bit;
+        bit <<= 1;
+      }
+
+      return result;
+    }
+  }
+}
