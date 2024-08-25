68ded26e87de97b244d956dd74d9adaf3e550d31
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2024-08-25 12:38:55 +0200

    0145. Binary Tree Postorder Traversal

---
+++ Solutions.cs/Binary Tree/0145. Binary Tree Postorder Traversal.cs
@@ -0,0 +1,38 @@
+﻿/// <summary>
+/// 145. Binary Tree Postorder Traversal
+/// https://leetcode.com/problems/binary-tree-postorder-traversal
+/// 
+/// Difficulty Easy
+/// Acceptance 72.5%
+/// 
+/// Stack
+/// Tree
+/// Depth-First Search
+/// Binary Tree
+/// </summary>
+public class Solution
+{
+  public IList<int> PostorderTraversal(TreeNode root)
+  {
+    var result = new List<int>();
+    var stack = new Stack<TreeNode>();
+
+    if (root != null)
+      stack.Push(root);
+
+    while (stack.TryPop(out var node))
+    {
+      if (node.left != null)
+        stack.Push(node.left);
+
+      if (node.right != null)
+        stack.Push(node.right);
+
+      result.Add(node.val);
+    }
+
+    result.Reverse();
+
+    return result;
+  }
+}
