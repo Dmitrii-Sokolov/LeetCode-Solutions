ac5be054e4391430c7a7bbc4d27718fcab79d9f0
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2024-09-01 08:22:56 +0200

    2022. Convert 1D Array Into 2D Array

---
+++ Solutions.cs/Graph/0743. Network Delay Time.cs
@@ -0,0 +1,65 @@
+﻿namespace Problem743
+{
+
+  /// <summary>
+  /// 743. Network Delay Time
+  /// https://leetcode.com/problems/network-delay-time
+  /// 
+  /// Difficulty Medium
+  /// Acceptance 54.9%
+  /// 
+  /// Depth-First Search
+  /// Breadth - First Search
+  /// Graph
+  /// Heap (Priority Queue)
+  /// Shortest Path
+  /// </summary>
+  public class Solution
+  {
+    public int NetworkDelayTime(int[][] times, int count, int k)
+    {
+      var edges = new Dictionary<int, List<(int Node, int Delay)>>();
+      foreach (var time in times)
+      {
+        var a = time[0];
+        var b = time[1];
+        var t = time[2];
+
+        AddEdge(edges, a, b, t);
+      }
+
+      var visited = new HashSet<int>();
+      var queue = new PriorityQueue<int, int>();
+      queue.Enqueue(k, 0);
+      while (queue.TryDequeue(out var node, out var delay))
+      {
+        if (visited.Contains(node))
+          continue;
+
+        visited.Add(node);
+
+        if (visited.Count == count)
+          return delay;
+
+        if (edges.TryGetValue(node, out var neighbours))
+        {
+          foreach (var neighbour in neighbours)
+            queue.Enqueue(neighbour.Node, neighbour.Delay + delay);
+        }
+      }
+
+      return -1;
+    }
+
+    private static void AddEdge(Dictionary<int, List<(int Node, int Delay)>> edges, int from, int to, int delay)
+    {
+      if (!edges.TryGetValue(from, out var list))
+      {
+        list = [];
+        edges.Add(from, list);
+      }
+
+      list.Add((to, delay));
+    }
+  }
+}
---
+++ Solutions.cs/Matrix/2022. Convert 1D Array Into 2D Array.cs
@@ -0,0 +1,33 @@
+﻿namespace Problem2022
+{
+
+  /// <summary>
+  /// 2022. Convert 1D Array Into 2D Array
+  /// https://leetcode.com/problems/convert-1d-array-into-2d-array
+  /// 
+  /// Difficulty Easy
+  /// Acceptance 65.9%
+  /// 
+  /// Array
+  /// Matrix
+  /// Simulation
+  /// </summary>
+  public class Solution
+  {
+    public int[][] Construct2DArray(int[] original, int m, int n)
+    {
+      if (original.Length != m * n)
+        return [];
+
+      var result = new int[m][];
+      for (var x = 0; x < m; x++)
+      {
+        result[x] = new int[n];
+        for (var y = 0; y < n; y++)
+          result[x][y] = original[x * n + y];
+      }
+
+      return result;
+    }
+  }
+}
