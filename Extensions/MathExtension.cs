b254fb0f923a240ef13e8b9e68c3c22fe1c03220
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2024-09-04 11:03:09 +0200

    0874. Walking Robot Simulation

--- Extensions/MathExtension.cs
+++ Extensions/MathExtension.cs
@@ -2,29 +2,44 @@
 {
   public static class Math
   {
-    private static int Mod(int x, int m) => ((x % m) + m) % m;
+    private static int Mod(int x, int m) => (x % m + m) % m;
   }
 
-  public struct Vector2 : IEquatable<Vector2>
+  public struct Vector2(int x, int y) : IEquatable<Vector2>
   {
-    public int X;
-    public int Y;
+    public readonly static Vector2 Zero = new(0, 0);
 
-    public Vector2(int x, int y)
-    {
-      X = x;
-      Y = y;
-    }
+    public readonly static Vector2 Right = new(1, 0);
+    public readonly static Vector2 Up = new(0, 1);
+    public readonly static Vector2 Left = new(-1, 0);
+    public readonly static Vector2 Down = new(0, -1);
+
+    public readonly static Vector2 North = Up;
+    public readonly static Vector2 West = Right;
+    public readonly static Vector2 South = Down;
+    public readonly static Vector2 East = Left;
+
+    public int X = x;
+    public int Y = y;
+
+    public readonly int Sqr => X * X + Y * Y;
+
+    public void RotateRight() => (X, Y) = (Y, -X);
+
+    public void RotateLeft() => (X, Y) = (-Y, X);
 
     public static implicit operator int[](Vector2 v) => [v.X, v.Y];
+    public static implicit operator Vector2(int[] v) => new(v[0], v[1]);
 
     public static Vector2 operator +(Vector2 a, Vector2 b) => new(a.X + b.X, a.Y + b.Y);
+    public static Vector2 operator -(Vector2 a, Vector2 b) => new(a.X - b.X, a.Y - b.Y);
 
     public static bool operator ==(Vector2 a, Vector2 b) => a.Equals(b);
     public static bool operator !=(Vector2 a, Vector2 b) => !a.Equals(b);
 
-    public override bool Equals(object obj) => obj is Vector2 v && Equals(v);
-    public bool Equals(Vector2 other) => X == other.X && Y == other.Y;
-    public override int GetHashCode() => HashCode.Combine(X, Y);
+    public readonly bool Equals(Vector2 other) => X == other.X && Y == other.Y;
+
+    public override readonly bool Equals(object obj) => obj is Vector2 v && Equals(v);
+    public override readonly int GetHashCode() => HashCode.Combine(X, Y);
   }
 }
---
+++ Solutions.cs/Simulation/0874. Walking Robot Simulation.cs
@@ -0,0 +1,93 @@
+﻿namespace Problem874
+{
+
+  /// <summary>
+  /// 874. Walking Robot Simulation
+  /// https://leetcode.com/problems/walking-robot-simulation
+  /// 
+  /// Difficulty Medium
+  /// Acceptance 46.2%
+  /// 
+  /// Array
+  /// Hash Table
+  /// Simulation
+  /// </summary>
+  public class Solution
+  {
+    public int RobotSim(int[] commands, int[][] obstaclesRaw)
+    {
+      var result = 0;
+      var direction = Vector2.North;
+      var position = Vector2.Zero;
+      var obstacles = obstaclesRaw.Select(o => (Vector2)o).ToHashSet();
+
+      foreach (var command in commands)
+      {
+        switch (command)
+        {
+          case -2:
+            direction.RotateLeft();
+            break;
+
+          case -1:
+            direction.RotateRight();
+            break;
+
+          default:
+            for (var i = 0; i < command; i++)
+            {
+              var next = position + direction;
+
+              if (obstacles.Contains(next))
+                break;
+
+              position = next;
+              result = Math.Max(result, position.Sqr);
+            }
+
+            break;
+        }
+      }
+
+      return result;
+    }
+
+    public struct Vector2(int x, int y) : IEquatable<Vector2>
+    {
+      public readonly static Vector2 Zero = new(0, 0);
+
+      public readonly static Vector2 Right = new(1, 0);
+      public readonly static Vector2 Up = new(0, 1);
+      public readonly static Vector2 Left = new(-1, 0);
+      public readonly static Vector2 Down = new(0, -1);
+
+      public readonly static Vector2 North = Up;
+      public readonly static Vector2 West = Right;
+      public readonly static Vector2 South = Down;
+      public readonly static Vector2 East = Left;
+
+      public int X = x;
+      public int Y = y;
+
+      public readonly int Sqr => X * X + Y * Y;
+
+      public void RotateRight() => (X, Y) = (Y, -X);
+
+      public void RotateLeft() => (X, Y) = (-Y, X);
+
+      public static implicit operator int[](Vector2 v) => [v.X, v.Y];
+      public static implicit operator Vector2(int[] v) => new(v[0], v[1]);
+
+      public static Vector2 operator +(Vector2 a, Vector2 b) => new(a.X + b.X, a.Y + b.Y);
+      public static Vector2 operator -(Vector2 a, Vector2 b) => new(a.X - b.X, a.Y - b.Y);
+
+      public static bool operator ==(Vector2 a, Vector2 b) => a.Equals(b);
+      public static bool operator !=(Vector2 a, Vector2 b) => !a.Equals(b);
+
+      public readonly bool Equals(Vector2 other) => X == other.X && Y == other.Y;
+
+      public override readonly bool Equals(object obj) => obj is Vector2 v && Equals(v);
+      public override readonly int GetHashCode() => HashCode.Combine(X, Y);
+    }
+  }
+}
