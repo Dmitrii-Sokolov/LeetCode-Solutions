5d4f19c07f1737849164c7bfdcad26b40504d08e
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2024-09-05 08:20:23 +0200

    2028. Find Missing Observations

---
+++ Solutions.cs/Math/2028. Find Missing Observations.cs
@@ -0,0 +1,35 @@
+﻿namespace Problem2028
+{
+
+  /// <summary>
+  /// 2028. Find Missing Observations
+  /// https://leetcode.com/problems/find-missing-observations
+  /// 
+  /// Difficulty Medium
+  /// Acceptance 51.9%
+  /// 
+  /// Array
+  /// Math
+  /// Simulation
+  /// </summary>
+  public class Solution
+  {
+    public int[] MissingRolls(int[] rolls, int mean, int n)
+    {
+      var sum = (n + rolls.Length) * mean - rolls.Sum();
+      if (n * 6 < sum || sum < n)
+        return [];
+
+      var result = new int[n];
+      var average = sum / n;
+      var remainder = sum - average * n;
+      for (var i = 0; i < result.Length; i++)
+      {
+        result[i] = average + (remainder > 0 ? 1 : 0);
+        remainder--;
+      }
+
+      return result;
+    }
+  }
+}
