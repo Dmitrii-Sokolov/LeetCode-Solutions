b542142375a0aad3e8dbec276d132dd5c442c12c
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2024-09-07 08:09:04 +0200

    1367. Linked List in Binary Tree

---
+++ Solutions.cs/Binary Tree/1367. Linked List in Binary Tree.cs
@@ -0,0 +1,32 @@
+﻿namespace Problem1367
+{
+
+  /// <summary>
+  /// 1367. Linked List in Binary Tree
+  /// https://leetcode.com/problems/linked-list-in-binary-tree
+  /// 
+  /// Difficulty Medium
+  /// Acceptance 46.3%
+  /// 
+  /// Linked List
+  /// LTree
+  /// LDepth-First Search
+  /// LBreadth-First Search
+  /// LBinary Tree
+  /// </summary>
+  public class Solution
+  {
+    public bool IsSubPath(ListNode head, TreeNode root)
+    {
+      return IsPath(head, root) ||
+        root.left != null && IsSubPath(head, root.left) ||
+        root.right != null && IsSubPath(head, root.right);
+    }
+
+    private bool IsPath(ListNode listNode, TreeNode treeNode)
+        => listNode == null ||
+        treeNode != null &&
+        listNode.val == treeNode.val &&
+        (IsPath(listNode.next, treeNode.left) || IsPath(listNode.next, treeNode.right));
+  }
+}
