13c9b0d34fbb9afc7e4a615ed0667b83abb43226
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2024-09-11 08:01:51 +0200

    2220. Minimum Bit Flips to Convert Number

---
+++ Solutions.cs/Bit Manipulation/2220. Minimum Bit Flips to Convert Number.cs
@@ -0,0 +1,28 @@
+﻿namespace Problem2220
+{
+
+  /// <summary>
+  /// 2220. Minimum Bit Flips to Convert Number
+  /// https://leetcode.com/problems/minimum-bit-flips-to-convert-number
+  /// 
+  /// Difficulty Easy
+  /// Acceptance 86.6%
+  /// 
+  /// Bit Manipulation
+  /// </summary>
+  public class Solution
+  {
+    public int MinBitFlips(int start, int goal)
+    {
+      var difference = start ^ goal;
+      var result = 0;
+      while (difference > 0)
+      {
+        result += difference % 2;
+        difference /= 2;
+      }
+
+      return result;
+    }
+  }
+}
