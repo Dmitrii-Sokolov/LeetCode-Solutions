6314f0908c967573bb75b367f25ab5dadbafdcdd
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2024-09-12 08:39:09 +0200

    1684. Count the Number of Consistent Strings

---
+++ Solutions.cs/Hash Table/1684. Count the Number of Consistent Strings.cs
@@ -0,0 +1,25 @@
+﻿namespace Problem1684
+{
+
+  /// <summary>
+  /// 1684. Count the Number of Consistent Strings
+  /// https://leetcode.com/problems/count-the-number-of-consistent-strings
+  /// 
+  /// Difficulty Easy
+  /// Acceptance 85.7%
+  /// 
+  /// Array
+  /// Hash Table
+  /// String
+  /// Bit Manipulation
+  /// Counting
+  /// </summary>
+  public class Solution
+  {
+    public int CountConsistentStrings(string allowedRaw, string[] words)
+    {
+      var allowed = allowedRaw.ToHashSet();
+      return words.Count(w => w.All(allowed.Contains));
+    }
+  }
+}
