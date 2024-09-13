d76872d53037776c440c44a47017dce0fa1d6124
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2024-09-13 09:30:16 +0200

    1310. XOR Queries of a Subarray

---
+++ Solutions.cs/Prefix Sum/1310. XOR Queries of a Subarray.cs
@@ -0,0 +1,35 @@
+﻿namespace Problem1310
+{
+
+  /// <summary>
+  /// 1310. XOR Queries of a Subarray
+  /// https://leetcode.com/problems/xor-queries-of-a-subarray
+  /// 
+  /// Difficulty Medium
+  /// Acceptance 76.7%
+  /// 
+  /// Array
+  /// Bit Manipulation
+  /// Prefix Sum
+  /// </summary>
+  public class Solution
+  {
+    public int[] XorQueries(int[] numbers, int[][] queries)
+    {
+      var prefixes = new int[numbers.Length + 1];
+      prefixes[0] = 0;
+      for (var i = 0; i < numbers.Length; i++)
+        prefixes[i + 1] = prefixes[i] ^ numbers[i];
+
+      var result = new int[queries.Length];
+      for (var i = 0; i < queries.Length; i++)
+      {
+        var left = queries[i][0];
+        var right = queries[i][1];
+        result[i] = prefixes[right + 1] ^ prefixes[left];
+      }
+
+      return result;
+    }
+  }
+}
