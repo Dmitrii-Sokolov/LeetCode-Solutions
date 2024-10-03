7b2d2f0a122cb3bccb2e0578c95543402aaffe21
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2024-10-03 21:48:21 +0200

    1331. Rank Transform of an Array

---
+++ Solutions.cs/Hash Table/1331. Rank Transform of an Array.cs
@@ -0,0 +1,41 @@
+﻿namespace Problem1331
+{
+
+  /// <summary>
+  /// 1331. Rank Transform of an Array
+  /// https://leetcode.com/problems/rank-transform-of-an-array
+  /// 
+  /// Difficulty Easy
+  /// Acceptance 70.4%
+  /// 
+  /// Array
+  /// Hash Table
+  /// Sorting
+  /// </summary>
+  public class Solution
+  {
+    public int[] ArrayRankTransform(int[] array)
+    {
+      var temp = array.ToArray();
+      Array.Sort(temp);
+
+      var rank = 0;
+      var ranks = new Dictionary<int, int>();
+      var previous = int.MaxValue;
+      for (var i = 0; i < temp.Length; i++)
+      {
+        if (temp[i] != previous)
+        {
+          previous = temp[i];
+          rank++;
+          ranks.Add(previous, rank);
+        }
+      }
+
+      for (var i = 0; i < array.Length; i++)
+        array[i] = ranks[array[i]];
+
+      return array;
+    }
+  }
+}
