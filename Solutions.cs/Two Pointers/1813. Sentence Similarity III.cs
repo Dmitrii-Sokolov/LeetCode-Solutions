36e7ae974dc2802eb53b9d69b284a2b67a21b6d3
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2024-10-06 21:18:21 +0200

    1813. Sentence Similarity III

---
+++ Solutions.cs/Two Pointers/1813. Sentence Similarity III.cs
@@ -0,0 +1,38 @@
+﻿namespace Problem1813
+{
+
+  /// <summary>
+  /// 1813. Sentence Similarity III
+  /// https://leetcode.com/problems/sentence-similarity-iii
+  /// 
+  /// Difficulty Medium
+  /// Acceptance 43.9%
+  /// 
+  /// Array
+  /// Two Pointers
+  /// String
+  /// </summary>
+  public class Solution
+  {
+    public bool AreSentencesSimilar(string sentence1, string sentence2)
+    {
+      var words1 = sentence1.Split(' ');
+      var words2 = sentence2.Split(' ');
+
+      if (words1.Length == words2.Length)
+        return sentence1 == sentence2;
+
+      var minLength = Math.Min(words1.Length, words2.Length);
+      var start = 0;
+      var end = 1;
+
+      while (start < minLength && words1[start] == words2[start])
+        start++;
+
+      while (end <= minLength && words1[^end] == words2[^end])
+        end++;
+
+      return start > minLength - end;
+    }
+  }
+}
