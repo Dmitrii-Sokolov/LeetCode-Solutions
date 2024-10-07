59d25260923fe158331b2f97748b251b76c34c2e
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2024-10-07 06:46:31 +0200

    2696. Minimum String Length After Removing Substrings

---
+++ Solutions.cs/Stack/2696. Minimum String Length After Removing Substrings.cs
@@ -0,0 +1,44 @@
+﻿namespace Problem2696
+{
+
+  /// <summary>
+  /// 2696. Minimum String Length After Removing Substrings
+  /// https://leetcode.com/problems/minimum-string-length-after-removing-substrings
+  /// 
+  /// Difficulty Easy
+  /// Acceptance 73.5%
+  /// 
+  /// String
+  /// Stack
+  /// Simulation
+  /// </summary>
+  public class Solution
+  {
+    public int MinLength(string s)
+    {
+      var previous = new Stack<char>();
+
+      foreach (var c in s)
+      {
+        if (previous.Count == 0)
+        {
+          previous.Push(c);
+        }
+        else if (c == 'B' && previous.Peek() == 'A')
+        {
+          previous.Pop();
+        }
+        else if (c == 'D' && previous.Peek() == 'C')
+        {
+          previous.Pop();
+        }
+        else
+        {
+          previous.Push(c);
+        }
+      }
+
+      return previous.Count;
+    }
+  }
+}
