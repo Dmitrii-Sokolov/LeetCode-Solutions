751820d9dc5460a515b781d8ddd019b22d055918
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2024-10-08 06:45:16 +0200

    1963. Minimum Number of Swaps to Make the String Balanced

---
+++ Solutions.cs/Greedy/1963. Minimum Number of Swaps to Make the String Balanced.cs
@@ -0,0 +1,44 @@
+﻿namespace Problem1963
+{
+
+  /// <summary>
+  /// 1963. Minimum Number of Swaps to Make the String Balanced
+  /// https://leetcode.com/problems/minimum-number-of-swaps-to-make-the-string-balanced
+  /// 
+  /// Difficulty Medium
+  /// Acceptance 73.8%
+  /// 
+  /// Two Pointers
+  /// String
+  /// Stack
+  /// Greedy
+  /// </summary>
+  public class Solution
+  {
+    public int MinSwaps(string s)
+    {
+      var balance = 0;
+      var swaps = 0;
+
+      foreach (var c in s)
+      {
+        if (c == '[')
+        {
+          balance++;
+        }
+        else
+        {
+          balance--;
+        }
+
+        if (balance < 0)
+        {
+          balance += 2;
+          swaps += 1;
+        }
+      }
+
+      return swaps;
+    }
+  }
+}
