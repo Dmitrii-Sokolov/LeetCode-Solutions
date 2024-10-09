4ce95bf60466452c2c57911039e2c5c53314fa0b
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2024-10-09 07:30:10 +0200

    0921. Minimum Add to Make Parentheses Valid

---
+++ Solutions.cs/Greedy/0921. Minimum Add to Make Parentheses Valid.cs
@@ -0,0 +1,41 @@
+﻿namespace Problem921
+{
+
+  /// <summary>
+  /// 921. Minimum Add to Make Parentheses Valid
+  /// https://leetcode.com/problems/minimum-add-to-make-parentheses-valid
+  /// 
+  /// Difficulty Medium
+  /// Acceptance 74.8%
+  /// 
+  /// String
+  /// Stack
+  /// Greedy
+  /// </summary>
+  public class Solution
+  {
+    public int MinAddToMakeValid(string s)
+    {
+      var balance = 0;
+      var adds = 0;
+
+      foreach (var c in s)
+      {
+        if (c == '(')
+          balance++;
+        else
+        {
+          balance--;
+        }
+
+        if (balance < 0)
+        {
+          balance += 1;
+          adds += 1;
+        }
+      }
+
+      return adds + balance;
+    }
+  }
+}
