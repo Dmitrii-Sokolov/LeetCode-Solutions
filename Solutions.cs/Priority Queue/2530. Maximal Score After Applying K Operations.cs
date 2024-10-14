6ba9c96c2d7aba7fe5d6f98387c933e98884e0a3
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2024-10-14 07:03:36 +0200

    2530. Maximal Score After Applying K Operations

---
+++ Solutions.cs/Priority Queue/2530. Maximal Score After Applying K Operations.cs
@@ -0,0 +1,36 @@
+﻿namespace Problem2530
+{
+
+  /// <summary>
+  /// 2530. Maximal Score After Applying K Operations
+  /// https://leetcode.com/problems/maximal-score-after-applying-k-operations
+  /// 
+  /// Difficulty Medium
+  /// Acceptance 54.2%
+  /// 
+  /// Array
+  /// Greedy
+  /// Heap(Priority Queue)
+  /// </summary>
+  public class Solution
+  {
+    public long MaxKelements(int[] numbers, int k)
+    {
+      var result = 0L;
+      var queue = new PriorityQueue<int, int>();
+      foreach (var number in numbers)
+        queue.Enqueue(number, -number);
+
+      while (k > 0)
+      {
+        var number = queue.Dequeue();
+        result += number;
+        number = (number + 2) / 3;
+        queue.Enqueue(number, -number);
+        k--;
+      }
+
+      return result;
+    }
+  }
+}
