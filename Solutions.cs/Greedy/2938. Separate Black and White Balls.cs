37375794d86d75d53201a08b562e3d0fb8e71e50
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2024-10-15 08:09:50 +0200

    2938. Separate Black and White Balls

---
+++ Solutions.cs/Greedy/2938. Separate Black and White Balls.cs
@@ -0,0 +1,44 @@
+﻿namespace Problem2938
+{
+
+  /// <summary>
+  /// 2938. Separate Black and White Balls
+  /// https://leetcode.com/problems/separate-black-and-white-balls
+  /// 
+  /// Difficulty Medium
+  /// Acceptance 60.8%
+  /// 
+  /// Two Pointers
+  /// String
+  /// Greedy
+  /// </summary>
+  public class Solution
+  {
+    public long MinimumSteps(string s)
+    {
+      var pointer = 0;
+      var result = 0L;
+      var blackCount = 0L;
+
+      while (pointer < s.Length)
+      {
+        var whiteCount = 0;
+        while (pointer < s.Length && s[pointer] == '1')
+        {
+          blackCount++;
+          pointer++;
+        }
+
+        while (pointer < s.Length && s[pointer] == '0')
+        {
+          whiteCount++;
+          pointer++;
+        }
+
+        result += blackCount * whiteCount;
+      }
+
+      return result;
+    }
+  }
+}
