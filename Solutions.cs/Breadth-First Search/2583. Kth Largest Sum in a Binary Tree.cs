df50d75d198792cff8799057d993527ee6d41b48
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2024-10-22 06:56:08 +0200

    2583. Kth Largest Sum in a Binary Tree

---
+++ Solutions.cs/Breadth-First Search/2583. Kth Largest Sum in a Binary Tree.cs
@@ -0,0 +1,59 @@
+﻿namespace Problem2583
+{
+
+  /// <summary>
+  /// 2583. Kth Largest Sum in a Binary Tree
+  /// https://leetcode.com/problems/kth-largest-sum-in-a-binary-tree
+  /// 
+  /// Difficulty Medium
+  /// Acceptance 52.2%
+  /// 
+  /// Tree
+  /// Breadth-First Search
+  /// Sorting
+  /// Binary Tree
+  /// </summary>
+  public class Solution
+  {
+    public long KthLargestLevelSum(TreeNode root, int k)
+    {
+      var sums = new PriorityQueue<long, long>();
+      var level = new Queue<TreeNode>();
+      level.Enqueue(root);
+      while (level.Count > 0)
+      {
+        var sum = 0L;
+        var count = level.Count;
+
+        for (var i = 0; i < count; i++)
+        {
+          var node = level.Dequeue();
+          sum += node.val;
+
+          if (node.right != null)
+            level.Enqueue(node.right);
+
+          if (node.left != null)
+            level.Enqueue(node.left);
+        }
+
+        sums.Enqueue(sum, -sum);
+      }
+
+      if (sums.Count < k)
+        return -1;
+
+      while (sums.Count > 0)
+      {
+        if (--k == 0)
+          return sums.Dequeue();
+        else
+        {
+          sums.Dequeue();
+        }
+      }
+
+      return -2;
+    }
+  }
+}
