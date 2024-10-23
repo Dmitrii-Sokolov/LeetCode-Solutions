526acc8f1570a07002bdde64f3130c2c9ebaa888
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2024-10-23 07:36:23 +0200

    2641. Cousins in Binary Tree II

---
+++ Solutions.cs/Breadth-First Search/2641. Cousins in Binary Tree II.cs
@@ -0,0 +1,67 @@
+﻿namespace Problem2641
+{
+
+  /// <summary>
+  /// 2641. Cousins in Binary Tree II
+  /// https://leetcode.com/problems/cousins-in-binary-tree-ii
+  /// 
+  /// Difficulty Medium
+  /// Acceptance 71.8%
+  /// 
+  /// Hash Table
+  /// Tree
+  /// Depth-First Search
+  /// Breadth-First Search
+  /// Binary Tree
+  /// </summary>
+  public class Solution
+  {
+    public TreeNode ReplaceValueInTree(TreeNode root)
+    {
+      root.val = 0;
+      var level = new Queue<TreeNode>();
+      level.Enqueue(root);
+      while (level.Count > 0)
+      {
+        var rowSum = 0;
+        var count = level.Count;
+
+        for (var i = 0; i < count; i++)
+        {
+          var node = level.Dequeue();
+          var childSum = 0;
+
+          if (node.right != null)
+          {
+            childSum += node.right.val;
+            level.Enqueue(node.right);
+          }
+
+          if (node.left != null)
+          {
+            childSum += node.left.val;
+            level.Enqueue(node.left);
+          }
+
+          if (node.right != null)
+            node.right.val = childSum;
+
+          if (node.left != null)
+            node.left.val = childSum;
+
+          rowSum += childSum;
+        }
+
+        count = level.Count;
+        for (var i = 0; i < count; i++)
+        {
+          var node = level.Dequeue();
+          node.val = rowSum - node.val;
+          level.Enqueue(node);
+        }
+      }
+
+      return root;
+    }
+  }
+}
