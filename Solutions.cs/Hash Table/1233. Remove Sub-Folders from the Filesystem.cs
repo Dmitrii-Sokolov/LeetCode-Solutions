795a6eefe0d8912ecf312052d1c2a45d9fde2a63
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2024-10-25 07:34:59 +0200

    1233. Remove Sub-Folders from the Filesystem

---
+++ Solutions.cs/Hash Table/1233. Remove Sub-Folders from the Filesystem.cs
@@ -0,0 +1,40 @@
+﻿namespace Problem1233
+{
+
+  /// <summary>
+  /// 1233. Remove Sub-Folders from the Filesystem
+  /// https://leetcode.com/problems/remove-sub-folders-from-the-filesystem
+  /// 
+  /// Difficulty Medium
+  /// Acceptance 69.3%
+  /// 
+  /// Array
+  /// String
+  /// Depth-First Search
+  /// Trie
+  /// </summary>
+  public class Solution
+  {
+    public IList<string> RemoveSubfolders(string[] folders)
+    {
+      var foldersSet = folders.ToHashSet();
+      return folders
+        .Where(folder => IsUnique(foldersSet, folder))
+        .ToList();
+    }
+
+    private static bool IsUnique(HashSet<string> folders, string folder)
+    {
+      var parts = folder.Split('/');
+      var path = string.Empty;
+      for (var i = 1; i < parts.Length - 1; i++)
+      {
+        path += $"/{parts[i]}";
+        if (folders.Contains(path))
+          return false;
+      }
+
+      return true;
+    }
+  }
+}
