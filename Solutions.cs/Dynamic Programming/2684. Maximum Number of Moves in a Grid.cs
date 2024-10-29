3584ca73efa24cdf960df7bad049f73cbf7c1cc6
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2024-10-29 06:33:58 +0100

    2684. Maximum Number of Moves in a Grid

---
+++ Solutions.cs/Dynamic Programming/2684. Maximum Number of Moves in a Grid.cs
@@ -0,0 +1,54 @@
+﻿namespace Problem2684
+{
+
+  /// <summary>
+  /// 2684. Maximum Number of Moves in a Grid
+  /// https://leetcode.com/problems/maximum-number-of-moves-in-a-grid 
+  /// 
+  /// Difficulty Medium
+  /// Acceptance 52.6%
+  /// 
+  /// Array
+  /// Dynamic Programming
+  /// Matrix
+  /// </summary>
+  public class Solution
+  {
+    public int MaxMoves(int[][] grid)
+    {
+      var height = grid.Length;
+      var width = grid[0].Length;
+
+      var result = 1;
+      var subResults = Enumerable.Repeat(1, height).ToArray();
+      for (var x = 1; x < width; x++)
+      {
+        var next = new int[height];
+
+        for (var y = 0; y < height; y++)
+        {
+          Check(grid, subResults, next, x, y, y - 1);
+          Check(grid, subResults, next, x, y, y);
+          Check(grid, subResults, next, x, y, y + 1);
+
+          if (next[y] > result)
+            result = next[y];
+        }
+
+        subResults = next;
+      }
+
+      return result - 1;
+    }
+
+    private static void Check(int[][] grid, int[] subResults, int[] next, int x, int y, int yPrev)
+    {
+      if (0 <= yPrev &&
+        yPrev < grid.Length &&
+        subResults[yPrev] != 0 &&
+        grid[y][x] > grid[yPrev][x - 1] &&
+        subResults[yPrev] + 1 > next[y])
+        next[y] = subResults[yPrev] + 1;
+    }
+  }
+}
