21f9db44a64e4d8a70564f03a38c5a0365992cad
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2024-10-30 17:00:01 +0100

    0300. Longest Increasing Subsequence

---
+++ Solutions.cs/Binary Search/0300. Longest Increasing Subsequence.cs
@@ -0,0 +1,65 @@
+﻿namespace Problem0300
+{
+
+  /// <summary>
+  /// 300. Longest Increasing Subsequence
+  /// https://leetcode.com/problems/longest-increasing-subsequence/description/
+  /// 
+  /// Difficulty Medium
+  /// Acceptance 56.5%
+  /// 
+  /// Array
+  /// Binary Search
+  /// Dynamic Programming
+  /// Longest Increasing Subsequence
+  /// </summary>
+  public class Solution
+  {
+    public int LengthOfLIS(int[] numbers)
+    {
+      var ends = new List<int>();
+      ends.Add(numbers[0]);
+      for (var i = 1; i < numbers.Length; i++)
+      {
+        var number = numbers[i];
+        if (ends[^1] < number)
+        {
+          ends.Add(number);
+        }
+        else
+        {
+          var index = FindFirstGreaterOrEqual(ends, number);
+          ends[index] = number;
+        }
+      }
+
+      return ends.Count;
+    }
+
+    private static int FindFirstGreaterOrEqual(List<int> list, int number)
+    {
+      var min = 0;
+      var max = list.Count - 1;
+      while (min < max)
+      {
+        var middle = max + min >> 1;
+        var test = list[middle];
+
+        if (test == number)
+        {
+          return middle;
+        }
+        else if (test < number)
+        {
+          min = middle + 1;
+        }
+        else
+        {
+          max = middle;
+        }
+      }
+
+      return min;
+    }
+  }
+}
