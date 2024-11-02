228791f67200d6cea1c43ec0c13af40c96564820
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2024-11-02 07:58:18 +0100

    2490. Circular Sentence

---
+++ Solutions.cs/String/2490. Circular Sentence.cs
@@ -0,0 +1,26 @@
+﻿namespace Problem2490
+{
+
+  /// <summary>
+  /// 2490. Circular Sentence
+  /// https://leetcode.com/problems/circular-sentence
+  /// 
+  /// Difficulty Easy
+  /// Acceptance 68.4%
+  /// 
+  /// String
+  /// </summary>
+  public class Solution
+  {
+    public bool IsCircularSentence(string sentence)
+    {
+      for (var i = 0; i < sentence.Length; i++)
+      {
+        if (sentence[i] == ' ' && sentence[i - 1] != sentence[i + 1])
+          return false;
+      }
+
+      return sentence[0] == sentence[^1];
+    }
+  }
+}
