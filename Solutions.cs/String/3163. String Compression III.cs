a3881442e37c0bf345002f1c06a0bec062628900
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2024-11-04 07:08:25 +0100

    3163. String Compression III

---
+++ Solutions.cs/String/3163. String Compression III.cs
@@ -0,0 +1,44 @@
+﻿namespace Problem3163
+{
+
+  /// <summary>
+  /// 3163. String Compression III
+  /// https://leetcode.com/problems/string-compression-iii
+  /// 
+  /// Difficulty Medium
+  /// Acceptance 63.3%
+  /// 
+  /// String
+  /// </summary>
+  public class Solution
+  {
+    public string CompressedString(string word)
+    {
+      var result = new System.Text.StringBuilder();
+      var last = word[0];
+      var repeats = 1;
+      for (var i = 1; i < word.Length; i++)
+      {
+        var ch = word[i];
+        if (last == ch && repeats < 9)
+          repeats++;
+        else
+        {
+          result.Append(repeats);
+          result.Append(last);
+
+          repeats = 1;
+          last = ch;
+        }
+      }
+
+      if (repeats > 0)
+      {
+        result.Append(repeats);
+        result.Append(last);
+      }
+
+      return result.ToString();
+    }
+  }
+}
