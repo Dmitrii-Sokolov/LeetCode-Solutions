df54c33d2479338dbb30d6b904c9ab0659f94809
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2024-11-05 06:42:43 +0100

    2914. Minimum Number of Changes to Make Binary String Beautiful

---
+++ Solutions.cs/String/2914. Minimum Number of Changes to Make Binary String Beautiful.cs
@@ -0,0 +1,27 @@
+﻿namespace Problem2914
+{
+
+  /// <summary>
+  /// 2914. Minimum Number of Changes to Make Binary String Beautiful
+  /// https://leetcode.com/problems/minimum-number-of-changes-to-make-binary-string-beautiful
+  /// 
+  /// Difficulty Medium
+  /// Acceptance 71.0%
+  /// 
+  /// String
+  /// </summary>
+  public class Solution
+  {
+    public int MinChanges(string s)
+    {
+      var result = 0;
+      for (var i = 0; i < s.Length / 2; i++)
+      {
+        if (s[2 * i] != s[2 * i + 1])
+          result++;
+      }
+
+      return result;
+    }
+  }
+}
