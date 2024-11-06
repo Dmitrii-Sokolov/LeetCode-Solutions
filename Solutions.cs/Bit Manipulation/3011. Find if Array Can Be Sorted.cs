484027e63a781c9a3dd403c0a6f5bd65e181f64c
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2024-11-06 08:01:39 +0100

    3011. Find if Array Can Be Sorted

---
+++ Solutions.cs/Bit Manipulation/3011. Find if Array Can Be Sorted.cs
@@ -0,0 +1,57 @@
+﻿namespace Problem3011
+{
+
+  /// <summary>
+  /// 3011. Find if Array Can Be Sorted
+  /// https://leetcode.com/problems/find-if-array-can-be-sorted
+  /// 
+  /// Difficulty Medium
+  /// Acceptance 60.9%
+  /// 
+  /// Array
+  /// Bit Manipulation
+  /// Sorting
+  /// </summary>
+  public class Solution
+  {
+    public bool CanSortArray(int[] numbers)
+    {
+      var currentBitsNumber = 0;
+      var currentMax = 0;
+      var previousMax = 0;
+      for (var i = 0; i < numbers.Length; i++)
+      {
+        var number = numbers[i];
+        var bitsNumber = CountNumberOfSetBits(number);
+        if (currentBitsNumber == bitsNumber)
+        {
+          if (number > currentMax)
+            currentMax = number;
+        }
+        else
+        {
+          previousMax = currentMax;
+          currentMax = number;
+          currentBitsNumber = bitsNumber;
+        }
+
+        if (number < previousMax)
+          return false;
+      }
+
+      return true;
+    }
+
+    public static int CountNumberOfSetBits(int number)
+    {
+      var result = 0;
+      while (number > 0)
+      {
+        result += number % 2;
+        number >>= 1;
+      }
+
+      return result;
+    }
+  }
+}
