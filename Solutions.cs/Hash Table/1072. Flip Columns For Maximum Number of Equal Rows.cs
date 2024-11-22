cacfc6c4f5f35e30ede322e4b971e7d30e56c1de
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2024-11-22 14:22:44 +0100

    1072. Flip Columns For Maximum Number of Equal Rows

---
+++ Solutions.cs/Hash Table/1072. Flip Columns For Maximum Number of Equal Rows.cs
@@ -0,0 +1,33 @@
+﻿namespace Problem1072
+{
+
+  /// <summary>
+  /// 1072. Flip Columns For Maximum Number of Equal Rows
+  /// https://leetcode.com/problems/flip-columns-for-maximum-number-of-equal-rows
+  /// 
+  /// Difficulty Medium
+  /// Acceptance 71.4%
+  /// 
+  /// Array
+  /// Hash Table
+  /// Matrix
+  /// </summary>
+  public class Solution
+  {
+    public int MaxEqualRowsAfterFlips(int[][] matrix)
+    {
+      var counts = new Dictionary<string, int>();
+      for (var i = 0; i < matrix.Length; i++)
+      {
+        var builder = new System.Text.StringBuilder();
+        for (var j = 1; j < matrix[0].Length; j++)
+          builder.Append(matrix[i][0] == 0 ? matrix[i][j] : 1 - matrix[i][j]);
+
+        var key = builder.ToString();
+        counts[key] = counts.GetValueOrDefault(key, 0) + 1;
+      }
+
+      return counts.Values.Max();
+    }
+  }
+}
