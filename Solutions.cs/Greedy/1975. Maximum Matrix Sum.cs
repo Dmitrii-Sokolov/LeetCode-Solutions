26287a4cc28205c4a282a3973d13f283b80a9c6b
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2024-11-24 07:07:29 +0100

    1975. Maximum Matrix Sum

---
+++ Solutions.cs/Greedy/1975. Maximum Matrix Sum.cs
@@ -0,0 +1,41 @@
+﻿namespace Problem1975
+{
+
+  /// <summary>
+  /// 1975. Maximum Matrix Sum
+  /// https://leetcode.com/problems/maximum-matrix-sum
+  /// 
+  /// Difficulty Medium
+  /// Acceptance 58.2%
+  /// 
+  /// Array
+  /// Greedy
+  /// Matrix
+  /// </summary>
+  public class Solution
+  {
+    public long MaxMatrixSum(int[][] matrix)
+    {
+      var result = 0L;
+      var min = int.MaxValue;
+      var signs = 0;
+      foreach (var row in matrix)
+      {
+        foreach (var cell in row)
+        {
+          var value = Math.Abs(cell);
+          if (cell < 0)
+            signs++;
+
+          result += value;
+          if (min > value)
+            min = value;
+        }
+      }
+
+      return signs % 2 == 0
+        ? result
+        : result - 2 * min;
+    }
+  }
+}
