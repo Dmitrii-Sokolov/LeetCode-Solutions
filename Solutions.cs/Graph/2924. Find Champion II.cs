f7dffd1d35de5f76caf021d000657e27c7cece51
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2024-11-26 08:15:05 +0100

    2924. Find Champion II

---
+++ Solutions.cs/Graph/2924. Find Champion II.cs
@@ -0,0 +1,25 @@
+﻿namespace Problem2924
+{
+
+  /// <summary>
+  /// 2924. Find Champion II
+  /// https://leetcode.com/problems/find-champion-ii
+  /// 
+  /// Difficulty Medium
+  /// Acceptance 65.7%
+  /// 
+  /// Graph
+  /// </summary>
+  public class Solution
+  {
+    public int FindChampion(int n, int[][] edges)
+    {
+      var candidates = Enumerable.Range(0, n).ToHashSet();
+
+      foreach (var edge in edges)
+        candidates.Remove(edge[1]);
+
+      return candidates.Count == 1 ? candidates.First() : -1;
+    }
+  }
+}
