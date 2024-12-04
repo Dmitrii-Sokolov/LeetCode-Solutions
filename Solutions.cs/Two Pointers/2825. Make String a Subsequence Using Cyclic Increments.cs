77e97d40c638f3f11ba0d1733df5f66572fffffe
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2024-12-04 09:17:30 +0100

    2825. Make String a Subsequence Using Cyclic Increments

---
+++ Solutions.cs/Two Pointers/2825. Make String a Subsequence Using Cyclic Increments.cs
@@ -0,0 +1,32 @@
+﻿namespace Problem2825
+{
+
+  /// <summary>
+  /// 2825. Make String a Subsequence Using Cyclic Increments
+  /// https://leetcode.com/problems/make-string-a-subsequence-using-cyclic-increments
+  /// 
+  /// Difficulty Medium
+  /// Acceptance 58.5%
+  /// 
+  /// Two Pointers
+  /// String
+  /// </summary>
+  public class Solution
+  {
+    public bool CanMakeSubsequence(string str1, string str2)
+    {
+      var p1 = 0;
+      var p2 = 0;
+      while (p1 < str1.Length && p2 < str2.Length)
+      {
+        var diff = str2[p2] - str1[p1];
+        if (diff == 0 || diff == 1 || diff == -25)
+          p2++;
+
+        p1++;
+      }
+
+      return p2 == str2.Length;
+    }
+  }
+}
