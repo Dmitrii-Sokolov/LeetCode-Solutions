469095a8135d0ce9510553c33e0be5b8617ac608
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2024-12-07 17:25:35 +0100

    2554. Maximum Number of Integers to Choose From a Range I

---
+++ Solutions.cs/Greedy/2554. Maximum Number of Integers to Choose From a Range I.cs
@@ -0,0 +1,38 @@
+﻿namespace Problem2554
+{
+
+  /// <summary>
+  /// 2554. Maximum Number of Integers to Choose From a Range I
+  /// https://leetcode.com/problems/maximum-number-of-integers-to-choose-from-a-range-i
+  /// 
+  /// Difficulty Medium
+  /// Acceptance 63.6%
+  /// 
+  /// Array
+  /// Hash Table
+  /// Binary Search
+  /// Greedy
+  /// Sorting
+  /// </summary>
+  public class Solution
+  {
+    public int MaxCount(int[] bannedRaw, int n, int maxSum)
+    {
+      var banned = bannedRaw.ToHashSet();
+      var count = 0;
+      var number = 1;
+      while (number <= n && number <= maxSum)
+      {
+        if (!banned.Contains(number))
+        {
+          count++;
+          maxSum -= number;
+        }
+
+        number++;
+      }
+
+      return count;
+    }
+  }
+}
