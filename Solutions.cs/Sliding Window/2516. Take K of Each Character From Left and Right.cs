8cd1bd861cafcf37eac223db7f430824a5cc69ee
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2024-12-12 10:57:48 +0100

    2516. Take K of Each Character From Left and Right

---
+++ Solutions.cs/Sliding Window/2516. Take K of Each Character From Left and Right.cs
@@ -0,0 +1,73 @@
+﻿namespace Problem2516
+{
+
+  /// <summary>
+  /// 2516. Take K of Each Character From Left and Right
+  /// https://leetcode.com/problems/take-k-of-each-character-from-left-and-right
+  /// 
+  /// Difficulty Medium
+  /// Acceptance 45.2%  
+  /// 
+  /// Hash Table
+  /// String
+  /// Sliding Window
+  /// </summary>
+  public class Solution
+  {
+    private const int CharCount = 3;
+    private const int Complete = 0b111;
+
+    public int TakeCharacters(string s, int k)
+    {
+      if (k == 0)
+        return 0;
+
+      if (s.Length < k * CharCount)
+        return -1;
+
+      var counts = new int[CharCount];
+      var right = s.Length;
+      var status = 0;
+      while (right > 0 && status != Complete)
+      {
+        var c = s[--right];
+        var type = c - 'a';
+        counts[type]++;
+        if (counts[type] >= k)
+          status |= 1 << type;
+      }
+
+      if (status != Complete)
+        return -1;
+
+      var left = -1;
+      var result = s.Length - right + left + 1;
+      var bestPossible = k * CharCount;
+      while (right < s.Length && result != bestPossible)
+      {
+        var type = s[++left] - 'a';
+        if (++counts[type] == k)
+          status |= 1 << type;
+
+        type = s[right++] - 'a';
+        if (--counts[type] == k - 1)
+          status &= ~(1 << type);
+
+        if (status == Complete)
+        {
+          while (right < s.Length && counts[s[right] - 'a'] > k)
+          {
+            type = s[right++] - 'a';
+            --counts[type];
+          }
+        }
+
+        var candidate = s.Length - right + left + 1;
+        if (result > candidate)
+          result = candidate;
+      }
+
+      return result;
+    }
+  }
+}
--- Solutions.cs/Sliding Window/2779. Maximum Beauty of an Array After Applying Operation.cs
+++ Solutions.cs/Sliding Window/2779. Maximum Beauty of an Array After Applying Operation.cs
@@ -19,25 +19,76 @@
     {
       Array.Sort(numbers);
       var maxDifference = 2 * k;
-      var left = 0;
+      return PerformSlidingWindow(
+        (r) => r < numbers.Length,
+        (l, r) => numbers[r] - numbers[l] <= maxDifference,
+        (l, r) => r - l,
+        (r, c) => r < c);
+    }
+
+    private static int PerformSlidingWindow(
+      Func<int, bool> loopCheck,
+      Func<int, int, bool> isValid,
+      Func<int, int, int> getCandidate,
+      Func<int, int, bool> isBetter)
+    {
+      var left = -1;
       var right = 0;
       var result = 1;
-      while (right < numbers.Length)
+      while (loopCheck(right))
       {
-        var min = numbers[right] - maxDifference;
-        while (numbers[left] < min)
+        left++;
+
+        while (!isValid(left, right))
           left++;
 
-        var max = numbers[left] + maxDifference;
-        while (right < numbers.Length && numbers[right] <= max)
+        while (loopCheck(right) && isValid(left, right))
           right++;
 
-        var candidate = right - left;
-        if (result < candidate)
+        var candidate = getCandidate(left, right);
+        if (isBetter(result, candidate))
           result = candidate;
       }
 
       return result;
     }
+
+    //public int MaximumBeauty(int[] numbers, int k)
+    //{
+    //  Array.Sort(numbers);
+    //  var maxDifference = 2 * k;
+    //  var left = -1;
+    //  var right = 0;
+    //  var result = 1;
+    //  PerformSlidingWindow(
+    //    () => right < numbers.Length,
+    //    () => left++,
+    //    () => numbers[right] - numbers[left] <= maxDifference,
+    //    () => right++,
+    //    () => result = Math.Max(result, right - left));
+
+    //  return result;
+    //}
+
+    //private static void PerformSlidingWindow(
+    //  Func<bool> loopCheck,
+    //  Action leftShift,
+    //  Func<bool> isValid,
+    //  Action rightShift,
+    //  Action resultRecord)
+    //{
+    //  while (loopCheck())
+    //  {
+    //    leftShift();
+
+    //    while (!isValid())
+    //      leftShift();
+
+    //    while (loopCheck() && isValid())
+    //      rightShift();
+
+    //    resultRecord();
+    //  }
+    //}
   }
 }
