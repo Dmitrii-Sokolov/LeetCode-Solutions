818147a0cf01931d09103687e831215027af57a2
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2024-12-15 07:40:41 +0100

    0053. Maximum Subarray

---
+++ Solutions.cs/Dynamic Programming/0053. Maximum Subarray.cs
@@ -0,0 +1,35 @@
+﻿namespace Problem53
+{
+
+  /// <summary>
+  /// 53. Maximum Subarray
+  /// https://leetcode.com/problems/maximum-subarray/description/
+  /// 
+  /// Difficulty Medium
+  /// Acceptance 51.5%
+  /// 
+  /// Array
+  /// Divide and Conquer
+  /// Dynamic Programming
+  /// </summary>
+  public class Solution
+  {
+    public int MaxSubArray(int[] numbers)
+    {
+      var sum = 0;
+      var result = numbers[0];
+      foreach (var number in numbers)
+      {
+        sum += number;
+
+        if (result < sum)
+          result = sum;
+
+        if (sum < 0)
+          sum = 0;
+      }
+
+      return result;
+    }
+  }
+}
