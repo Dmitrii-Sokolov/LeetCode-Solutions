865b8d5d7b0627e20a8ae223bd142c89d13f44a8
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2024-12-16 10:06:28 +0100

    1942. The Number of the Smallest Unoccupied Chair

---
+++ Solutions.cs/Priority Queue/1942. The Number of the Smallest Unoccupied Chair.cs
@@ -0,0 +1,41 @@
+﻿namespace Problem1942
+{
+
+  /// <summary>
+  /// 1942. The Number of the Smallest Unoccupied Chair
+  /// https://leetcode.com/problems/the-number-of-the-smallest-unoccupied-chair
+  /// 
+  /// Difficulty Medium
+  /// Acceptance 52.1%
+  /// 
+  /// Array
+  /// Hash Table
+  /// Heap (Priority Queue)
+  /// </summary>
+  public class Solution
+  {
+    public int SmallestChair(int[][] friends, int targetFriend)
+    {
+      var targetArrival = friends[targetFriend][0];
+      Array.Sort(friends, (a, b) => a[0].CompareTo(b[0]));
+      var availableChairs = new PriorityQueue<int, int>();
+      var occupiedChairs = new PriorityQueue<int, int>();
+      for (var i = 0; i < friends.Length; i++)
+      {
+        var arrival = friends[i][0];
+        var leaving = friends[i][1];
+
+        while (occupiedChairs.Count > 0 && occupiedChairs.TryPeek(out var index, out var time) && time <= arrival)
+          availableChairs.Enqueue(index, occupiedChairs.Dequeue());
+
+        var chair = availableChairs.Count > 0 ? availableChairs.Dequeue() : occupiedChairs.Count;
+        if (arrival == targetArrival)
+          return chair;
+
+        occupiedChairs.Enqueue(chair, leaving);
+      }
+
+      return -1;
+    }
+  }
+}
