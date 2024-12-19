9c17e1cd80e391db29db22e6a24a185473cf2b23
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2024-12-19 07:43:07 +0100

    0769. Max Chunks To Make Sorted

---
+++ Solutions.cs/Sorting/0769. Max Chunks To Make Sorted.cs
@@ -0,0 +1,35 @@
+﻿namespace Problem769
+{
+
+  /// <summary>
+  /// 769. Max Chunks To Make Sorted
+  /// https://leetcode.com/problems/max-chunks-to-make-sorted
+  /// 
+  /// Difficulty Medium
+  /// Acceptance 60.4%
+  /// 
+  /// Array
+  /// Stack
+  /// Greedy
+  /// Sorting
+  /// Monotonic Stack
+  /// </summary>
+  public class Solution
+  {
+    public int MaxChunksToSorted(int[] numbers)
+    {
+      var result = 0;
+      var sum = 0;
+      var targetSum = 0;
+      for (var i = 0; i < numbers.Length; i++)
+      {
+        sum += i;
+        targetSum += numbers[i];
+        if (sum == targetSum)
+          result++;
+      }
+
+      return result;
+    }
+  }
+}
