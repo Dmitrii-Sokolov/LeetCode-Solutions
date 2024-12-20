795e3b116cc123e9289d8af5ea562fb9c2a7638c
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2024-12-20 07:50:39 +0100

    2415. Reverse Odd Levels of Binary Tree

---
+++ Solutions.cs/Breadth-First Search/2415. Reverse Odd Levels of Binary Tree.cs
@@ -0,0 +1,36 @@
+﻿namespace Problem2415
+{
+
+  /// <summary>
+  /// 2415. Reverse Odd Levels of Binary Tree
+  /// https://leetcode.com/problems/reverse-odd-levels-of-binary-tree
+  /// 
+  /// Difficulty Medium
+  /// Acceptance 82.2%
+  /// 
+  /// Tree
+  /// Depth-First Search
+  /// Breadth-First Search
+  /// Binary Tree
+  /// </summary>
+  public class Solution
+  {
+    public TreeNode ReverseOddLevels(TreeNode root)
+    {
+      Reverse(root.left, root.right, true);
+      return root;
+    }
+
+    private static void Reverse(TreeNode left, TreeNode right, bool isOdd)
+    {
+      if (left == null)
+        return;
+
+      if (isOdd)
+        (left.val, right.val) = (right.val, left.val);
+
+      Reverse(left.left, right.right, !isOdd);
+      Reverse(left.right, right.left, !isOdd);
+    }
+  }
+}
