845c4f7a572d040bed501255a7fa1c45b35e8b51
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2024-12-26 12:19:09 +0100

    0494. Target Sum

---
+++ Solutions.cs/Dynamic Programming/0494. Target Sum.cs
@@ -0,0 +1,47 @@
+﻿namespace Problem494
+{
+
+  /// <summary>
+  /// 494. Target Sum
+  /// https://leetcode.com/problems/target-sum
+  /// 
+  /// Difficulty Medium
+  /// Acceptance 48.5%
+  /// 
+  /// Array
+  /// Dynamic Programming
+  /// Backtracking
+  /// </summary>
+  public class Solution
+  {
+    public int FindTargetSumWays(int[] numbers, int target)
+    {
+      var sum = 0;
+      for (var i = 0; i < numbers.Length; i++)
+        sum += numbers[i];
+
+      if ((target + sum) % 2 != 0)
+        return 0;
+
+      if (target < 0)
+        target = -target;
+
+      if (target > sum)
+        return 0;
+
+      var max = 1;
+      var results = new int[sum + 1];
+      results[0] = 1;
+      for (var i = 0; i < numbers.Length; i++)
+      {
+        var number = numbers[i];
+        max += number;
+
+        for (var j = max - 1; j >= number; j--)
+          results[j] += results[j - number];
+      }
+
+      return results[(sum + target) / 2];
+    }
+  }
+}
