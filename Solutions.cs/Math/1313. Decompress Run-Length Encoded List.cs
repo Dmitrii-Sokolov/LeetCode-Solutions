bc7a2d9262c718b0c2dbc7cde03ae9473ecdce84
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2024-12-27 15:18:48 +0100

    1313. Decompress Run-Length Encoded List.cs

---
+++ Solutions.cs/Math/1313. Decompress Run-Length Encoded List.cs
@@ -0,0 +1,30 @@
+﻿namespace Problem1313
+{
+
+  /// <summary>
+  /// 1313. Decompress Run-Length Encoded List
+  /// https://leetcode.com/problems/decompress-run-length-encoded-list
+  /// 
+  /// Difficulty Easy
+  /// Acceptance 85.9%
+  /// 
+  /// Array
+  /// </summary>
+  public class Solution
+  {
+    public int[] DecompressRLElist(int[] codes)
+    {
+      var result = new List<int>();
+      var pointer = 0;
+      while (pointer < codes.Length)
+      {
+        var repeats = codes[pointer++];
+        var number = codes[pointer++];
+        while (repeats-- > 0)
+          result.Add(number);
+      }
+
+      return result.ToArray();
+    }
+  }
+}
