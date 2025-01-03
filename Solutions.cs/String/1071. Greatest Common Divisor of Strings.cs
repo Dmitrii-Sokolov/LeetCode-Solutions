586a2784e6cff59aee09aedb39ddbe51bba823c0
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2025-01-03 11:05:34 +0100

    1071. Greatest Common Divisor of Strings

---
+++ Solutions.cs/String/1071. Greatest Common Divisor of Strings.cs
@@ -0,0 +1,36 @@
+﻿namespace Problem1071
+{
+
+  /// <summary>
+  /// 1071. Greatest Common Divisor of Strings
+  /// https://leetcode.com/problems/greatest-common-divisor-of-strings
+  /// 
+  /// Difficulty Easy
+  /// Acceptance 52.3%
+  /// 
+  /// Math
+  /// String
+  /// </summary>
+  public class Solution
+  {
+    public string GcdOfStrings(string str1, string str2)
+    {
+      if (str1 + str2 != str2 + str1)
+        return string.Empty;
+
+      var gcd = GetGCD(str1.Length, str2.Length);
+      return str1[..gcd];
+    }
+
+    private static int GetGCD(int a, int b)
+    {
+      while (b != 0)
+      {
+        a %= b;
+        (a, b) = (b, a);
+      }
+
+      return a;
+    }
+  }
+}
