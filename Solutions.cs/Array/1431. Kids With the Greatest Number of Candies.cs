65de601e5424e6b13b21c85cea792b7b31be6201
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2025-01-04 11:54:30 +0100

    1431. Kids With the Greatest Number of Candies

---
+++ Solutions.cs/Array/1431. Kids With the Greatest Number of Candies.cs
@@ -0,0 +1,32 @@
+﻿namespace Problem1431
+{
+
+  /// <summary>
+  /// 1431. Kids With the Greatest Number of Candies
+  /// https://leetcode.com/problems/kids-with-the-greatest-number-of-candies
+  /// 
+  /// Difficulty Easy
+  /// Acceptance 87.9%
+  /// 
+  /// Array
+  /// </summary>
+  public class Solution
+  {
+    public IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+    {
+      var max = 0;
+      foreach (var count in candies)
+      {
+        if (max < count)
+          max = count;
+      }
+
+      max -= extraCandies;
+      var result = new List<bool>(candies.Length);
+      foreach (var count in candies)
+        result.Add(count >= max);
+
+      return result;
+    }
+  }
+}
