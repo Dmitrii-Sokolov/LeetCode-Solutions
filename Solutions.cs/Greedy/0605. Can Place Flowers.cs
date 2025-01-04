091b61fc85924b0dde78efc1708ce985fa05c4cd
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2025-01-04 12:04:07 +0100

    0605. Can Place Flowers

---
+++ Solutions.cs/Greedy/0605. Can Place Flowers.cs
@@ -0,0 +1,46 @@
+﻿namespace Problem605
+{
+
+  /// <summary>
+  /// 605. Can Place Flowers
+  /// https://leetcode.com/problems/can-place-flowers
+  /// 
+  /// Difficulty Easy
+  /// Acceptance 28.8%
+  /// 
+  /// Array
+  /// Greedy
+  /// </summary>
+  public class Solution
+  {
+    public bool CanPlaceFlowers(int[] flowerbed, int n)
+    {
+      var empty = 1;
+      for (var i = 0; i < flowerbed.Length; i++)
+      {
+        if (flowerbed[i] == 0)
+        {
+          empty++;
+        }
+        else
+        {
+          empty = 0;
+        }
+
+        if (empty == 3)
+        {
+          empty = 1;
+          n--;
+        }
+
+        if (n == 0)
+          return true;
+      }
+
+      if (empty == 2)
+        n--;
+
+      return n == 0;
+    }
+  }
+}
