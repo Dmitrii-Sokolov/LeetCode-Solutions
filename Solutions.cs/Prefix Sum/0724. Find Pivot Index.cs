bf6ad7427643dd59123c187c757b097f89855de4
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2025-01-05 16:19:37 +0100

    0724. Find Pivot Index

---
+++ Solutions.cs/Prefix Sum/0724. Find Pivot Index.cs
@@ -0,0 +1,36 @@
+﻿namespace Problem724
+{
+
+  /// <summary>
+  /// 724. Find Pivot Index
+  /// https://leetcode.com/problems/find-pivot-index
+  /// 
+  /// Difficulty Easy
+  /// Acceptance 59.4%
+  /// 
+  /// Array
+  /// Prefix Sum
+  /// </summary>
+  public class Solution
+  {
+    public int PivotIndex(int[] numbers)
+    {
+      var sumRight = 0;
+      for (var i = 0; i < numbers.Length; i++)
+        sumRight += numbers[i];
+
+      var sumLeft = 0;
+      for (var i = 0; i < numbers.Length; i++)
+      {
+        sumRight -= numbers[i];
+
+        if (sumLeft == sumRight)
+          return i;
+
+        sumLeft += numbers[i];
+      }
+
+      return -1;
+    }
+  }
+}
