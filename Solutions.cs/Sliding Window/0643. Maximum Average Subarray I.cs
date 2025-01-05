0033008ea5d5e6e97971abacbf9713784360d9d6
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2025-01-05 09:33:42 +0100

    0643. Maximum Average Subarray I

---
+++ Solutions.cs/Sliding Window/0643. Maximum Average Subarray I.cs
@@ -0,0 +1,35 @@
+﻿namespace Problem643
+{
+
+  /// <summary>
+  /// 643. Maximum Average Subarray I
+  /// https://leetcode.com/problems/maximum-average-subarray-i
+  /// 
+  /// Difficulty Easy
+  /// Acceptance 44.3%
+  /// 
+  /// Array
+  /// Sliding Window
+  /// </summary>
+  public class Solution
+  {
+    public double FindMaxAverage(int[] numbers, int k)
+    {
+      var sum = 0d;
+      for (var i = 0; i < k; i++)
+        sum += numbers[i];
+
+      var result = sum;
+      for (var i = k; i < numbers.Length; i++)
+      {
+        sum += numbers[i];
+        sum -= numbers[i - k];
+
+        if (result < sum)
+          result = sum;
+      }
+
+      return result / k;
+    }
+  }
+}
