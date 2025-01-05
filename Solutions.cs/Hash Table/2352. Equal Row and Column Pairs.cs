5c66de041d61aa89d9845c688ff4dcc9f956b480
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2025-01-05 21:14:33 +0100

    2352. Equal Row and Column Pairs

---
+++ Solutions.cs/Hash Table/2352. Equal Row and Column Pairs.cs
@@ -0,0 +1,48 @@
+﻿namespace Problem2352
+{
+
+  /// <summary>
+  /// 2352. Equal Row and Column Pairs
+  /// https://leetcode.com/problems/equal-row-and-column-pairs
+  /// 
+  /// Difficulty Medium
+  /// Acceptance 70.3%
+  /// 
+  /// Array
+  /// Hash Table
+  /// Matrix
+  /// Simulation
+  /// </summary>
+  public class Solution
+  {
+    public int EqualPairs(int[][] grid)
+    {
+      var n = grid.Length;
+      if (n == 1)
+        return 1;
+
+      var xHashes = new int[n];
+      var yHashes = new int[n];
+      for (var j = 0; j < n; j++)
+      {
+        for (var i = 0; i < n; i++)
+        {
+          yHashes[j] = yHashes[j] * 7 + grid[i][j];
+          xHashes[j] = xHashes[j] * 7 + grid[j][i];
+        }
+      }
+
+      var result = 0;
+      for (var x = 0; x < n; x++)
+      {
+        for (var y = 0; y < n; y++)
+        {
+          if (xHashes[x] == yHashes[y])
+            result++;
+        }
+      }
+
+      return result;
+    }
+  }
+}
