18c2b1c0cb05f87c86ddca0a11bfeac7d579f7c1
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2025-01-05 16:16:25 +0100

    1732. Find the Highest Altitude

---
+++ Solutions.cs/Prefix Sum/1732. Find the Highest Altitude.cs
@@ -0,0 +1,31 @@
+﻿namespace Problem1732
+{
+
+  /// <summary>
+  /// 1732. Find the Highest Altitude
+  /// https://leetcode.com/problems/find-the-highest-altitude
+  /// 
+  /// Difficulty Easy
+  /// Acceptance 83.7%
+  /// 
+  /// Array
+  /// Prefix Sum
+  /// </summary>
+  public class Solution
+  {
+    public int LargestAltitude(int[] gain)
+    {
+      var altitude = 0;
+      var result = 0;
+      foreach (var delta in gain)
+      {
+        altitude += delta;
+
+        if (result < altitude)
+          result = altitude;
+      }
+
+      return result;
+    }
+  }
+}
