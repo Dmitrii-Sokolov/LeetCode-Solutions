1784f7248c087b40668f69ee957ba25f3365636e
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2025-01-07 07:30:24 +0100

    1408. String Matching in an Array

---
+++ Solutions.cs/String/1408. String Matching in an Array.cs
@@ -0,0 +1,40 @@
+﻿namespace Problem1408
+{
+
+  /// <summary>
+  /// 1408. String Matching in an Array
+  /// https://leetcode.com/problems/string-matching-in-an-array
+  /// 
+  /// Difficulty Easy
+  /// Acceptance 66.3%
+  /// 
+  /// Array
+  /// String
+  /// String Matching
+  /// </summary>
+  public class Solution
+  {
+    public IList<string> StringMatching(string[] words)
+    {
+      var result = new List<string>(words.Length);
+      for (var i = 0; i < words.Length; i++)
+      {
+        if (IsSubstring(words[i], words))
+          result.Add(words[i]);
+      }
+
+      return result;
+    }
+
+    private static bool IsSubstring(string word, string[] words)
+    {
+      foreach (var anotherWord in words)
+      {
+        if (word.Length < anotherWord.Length && anotherWord.Contains(word))
+          return true;
+      }
+
+      return false;
+    }
+  }
+}
