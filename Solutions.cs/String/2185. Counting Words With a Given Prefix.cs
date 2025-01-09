9456a8707cafdfa8f3aefd75f7a79b3e179fb835
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2025-01-09 07:10:02 +0100

    2185. Counting Words With a Given Prefix

---
+++ Solutions.cs/String/2185. Counting Words With a Given Prefix.cs
@@ -0,0 +1,29 @@
+﻿namespace Problem2185
+{
+
+  /// <summary>
+  /// 2185. Counting Words With a Given Prefix
+  /// https://leetcode.com/problems/counting-words-with-a-given-prefix
+  /// 
+  /// Difficulty Easy
+  /// Acceptance 81.8%
+  /// 
+  /// Array
+  /// String
+  /// String Matching
+  /// </summary>
+  public class Solution
+  {
+    public int PrefixCount(string[] words, string prefix)
+    {
+      var result = 0;
+      foreach (var word in words)
+      {
+        if (word.Length >= prefix.Length && word.StartsWith(prefix))
+          result++;
+      }
+
+      return result;
+    }
+  }
+}
