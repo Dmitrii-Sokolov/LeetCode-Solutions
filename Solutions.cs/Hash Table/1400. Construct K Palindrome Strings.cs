241231e20093a52d2355831e317a061124819b09
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2025-01-11 10:53:42 +0100

    1400. Construct K Palindrome Strings

---
+++ Solutions.cs/Hash Table/1400. Construct K Palindrome Strings.cs
@@ -0,0 +1,40 @@
+﻿namespace Problem1400
+{
+
+  /// <summary>
+  /// 1400. Construct K Palindrome Strings
+  /// https://leetcode.com/problems/construct-k-palindrome-strings
+  /// 
+  /// Difficulty Medium
+  /// Acceptance 65.8%
+  /// 
+  /// Hash Table
+  /// String
+  /// Greedy
+  /// Counting
+  /// </summary>
+  public class Solution
+  {
+    public bool CanConstruct(string s, int k)
+    {
+      if (s.Length < k)
+        return false;
+
+      if (k >= 26)
+        return true;
+
+      var isOdd = new bool[26];
+      foreach (var c in s)
+        isOdd[c - 'a'] = !isOdd[c - 'a'];
+
+      var oddCount = 0;
+      foreach (var c in isOdd)
+      {
+        if (c)
+          oddCount++;
+      }
+
+      return oddCount <= k;
+    }
+  }
+}
