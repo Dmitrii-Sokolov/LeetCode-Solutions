61b3651f4571ef829d0d6d065c1cdd2a292e7e42
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2025-01-17 11:38:08 +0100

    2683. Neighboring Bitwise XOR

---
+++ Solutions.cs/Bit Manipulation/2683. Neighboring Bitwise XOR.cs
@@ -0,0 +1,25 @@
+﻿namespace Problem2683
+{
+
+  /// <summary>
+  /// 2683. Neighboring Bitwise XOR
+  /// https://leetcode.com/problems/neighboring-bitwise-xor
+  /// 
+  /// Difficulty Medium
+  /// Acceptance 74.6%
+  /// 
+  /// Array
+  /// Bit Manipulation
+  /// </summary>
+  public class Solution
+  {
+    public bool DoesValidArrayExist(int[] derived)
+    {
+      var xor = 0;
+      foreach (var number in derived)
+        xor ^= number;
+
+      return xor == 0;
+    }
+  }
+}
