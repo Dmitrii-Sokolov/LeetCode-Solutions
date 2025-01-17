612993de7624dbdf82fc313d17d6defce1b4aca2
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2025-01-17 16:59:05 +0100

    0933. Number of Recent Calls

---
+++ Solutions.cs/Design/0933. Number of Recent Calls.cs
@@ -0,0 +1,30 @@
+﻿namespace Problem933
+{
+
+  /// <summary>
+  /// 933. Number of Recent Calls
+  /// https://leetcode.com/problems/number-of-recent-calls
+  /// 
+  /// Difficulty Easy
+  /// Acceptance 76.8%
+  /// 
+  /// Design
+  /// Queue
+  /// Data Stream
+  /// </summary>
+  public class RecentCounter
+  {
+    private const int Window = 3000;
+
+    private Queue<int> Queue = new();
+
+    public int Ping(int t)
+    {
+      Queue.Enqueue(t);
+      while (Queue.Peek() + Window < t)
+        Queue.Dequeue();
+
+      return Queue.Count;
+    }
+  }
+}
