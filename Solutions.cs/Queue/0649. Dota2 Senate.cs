d548fff790701bbb6d28a31a3c6ef15990125ad9
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2025-01-18 08:06:55 +0100

    0649. Dota2 Senate

---
+++ Solutions.cs/Queue/0649. Dota2 Senate.cs
@@ -0,0 +1,72 @@
+﻿namespace Problem649
+{
+
+  /// <summary>
+  /// 649. Dota2 Senate
+  /// https://leetcode.com/problems/dota2-senate
+  /// 
+  /// Difficulty Medium
+  /// Acceptance 48.3%
+  /// 
+  /// String
+  /// Greedy
+  /// Queue
+  /// </summary>
+  public class Solution
+  {
+    private const char Radiant = 'R';
+    private const char Dire = 'D';
+
+    public string PredictPartyVictory(string senate)
+    {
+      var queue = new Queue<char>();
+      var radiantRemain = 0;
+      var direRemain = 0;
+      foreach (var member in senate)
+      {
+        queue.Enqueue(member);
+        if (member == Radiant)
+        {
+          radiantRemain++;
+        }
+        else
+        {
+          direRemain++;
+        }
+      }
+
+      var balance = 0;
+      while (radiantRemain > 0 && direRemain > 0)
+      {
+        if (queue.Dequeue() == Radiant)
+        {
+          if (balance > 0)
+          {
+            radiantRemain--;
+          }
+          else
+          {
+            queue.Enqueue(Radiant);
+          }
+
+          balance--;
+        }
+        else
+        {
+          if (balance < 0)
+          {
+            direRemain--;
+          }
+          else
+          {
+            queue.Enqueue(Dire);
+          }
+
+          balance++;
+        }
+      }
+
+      return radiantRemain > 0 ? nameof(Radiant) : nameof(Dire);
+    }
+  }
+}
