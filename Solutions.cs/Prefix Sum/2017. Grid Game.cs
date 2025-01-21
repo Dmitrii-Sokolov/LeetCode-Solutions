7a97ddde238cb954a7a6ac6afe33a5439467650c
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2025-01-21 08:50:40 +0100

    2017. Grid Game

---
+++ Solutions.cs/Prefix Sum/2017. Grid Game.cs
@@ -0,0 +1,39 @@
+﻿namespace Problem2017
+{
+
+  /// <summary>
+  /// 2017. Grid Game
+  /// https://leetcode.com/problems/grid-game
+  /// 
+  /// Difficulty Medium
+  /// Acceptance 53.3%
+  /// 
+  /// Array
+  /// Matrix
+  /// Prefix Sum
+  /// </summary>
+  public class Solution
+  {
+    public long GridGame(int[][] grid)
+    {
+      var result = long.MaxValue;
+      var prefixTop = 0L;
+      var prefixBottom = 0L;
+      for (var i = 0; i < grid[0].Length; i++)
+        prefixTop += grid[0][i];
+
+      for (var i = 0; i < grid[0].Length; i++)
+      {
+        prefixTop -= grid[0][i];
+
+        var prefix = Math.Max(prefixTop, prefixBottom);
+        if (result > prefix)
+          result = prefix;
+
+        prefixBottom += grid[1][i];
+      }
+
+      return result;
+    }
+  }
+}
