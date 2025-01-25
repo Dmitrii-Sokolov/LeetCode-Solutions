5c66c985c813edca6a4bf009f2bd48ad38d8c0a3
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2025-01-25 12:40:31 +0100

    2948. Make Lexicographically Smallest Array by Swapping Elements

---
+++ Solutions.cs/Queue/2948. Make Lexicographically Smallest Array by Swapping Elements.cs
@@ -0,0 +1,40 @@
+﻿namespace Problem2948
+{
+
+  /// <summary>
+  /// 2948. Make Lexicographically Smallest Array by Swapping Elements
+  /// https://leetcode.com/problems/make-lexicographically-smallest-array-by-swapping-elements
+  /// 
+  /// Difficulty Medium
+  /// Acceptance 51.1%
+  /// 
+  /// Array
+  /// Union Find
+  /// Sorting
+  /// </summary>
+  public class Solution
+  {
+    public int[] LexicographicallySmallestArray(int[] numbers, int limit)
+    {
+      var sortedClone = numbers.ToArray();
+      Array.Sort(sortedClone);
+
+      var queues = new Dictionary<int, Queue<int>>();
+      var queue = new Queue<int>();
+
+      for (var i = 0; i < numbers.Length; i++)
+      {
+        queues[sortedClone[i]] = queue;
+        queue.Enqueue(sortedClone[i]);
+
+        if (i == numbers.Length - 1 || sortedClone[i + 1] - limit > sortedClone[i])
+          queue = new Queue<int>();
+      }
+
+      for (var i = 0; i < numbers.Length; i++)
+        numbers[i] = queues[numbers[i]].Dequeue();
+
+      return numbers;
+    }
+  }
+}
