9f3d64685eaee16a46bd017f2c191f6dec539706
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2025-02-01 07:58:29 +0100

    3151. Special Array I

---
+++ Solutions.cs/Array/3151. Special Array I.cs
@@ -0,0 +1,28 @@
+﻿namespace Problem3151
+{
+
+  /// <summary>
+  /// 3151. Special Array I
+  /// https://leetcode.com/problems/special-array-i
+  /// 
+  /// Difficulty Easy
+  /// Acceptance 79.3%
+  /// 
+  /// Array
+  /// </summary>
+  public class Solution
+  {
+    public bool IsArraySpecial(int[] numbers)
+    {
+      var bit = numbers[0] & 1;
+
+      for (var i = 1; i < numbers.Length; i++)
+      {
+        if ((numbers[i] + i & 1) != bit)
+          return false;
+      }
+
+      return true;
+    }
+  }
+}
