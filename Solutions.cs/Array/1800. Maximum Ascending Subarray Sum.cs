ac527019b31b15612563e9160a36df523d02a432
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2025-02-04 09:31:49 +0100

    1800. Maximum Ascending Subarray Sum

---
+++ Solutions.cs/Array/1800. Maximum Ascending Subarray Sum.cs
@@ -0,0 +1,39 @@
+﻿namespace Problem1800
+{
+
+  /// <summary>
+  /// 1800. Maximum Ascending Subarray Sum
+  /// https://leetcode.com/problems/maximum-ascending-subarray-sum
+  /// 
+  /// Difficulty Easy
+  /// Acceptance 65.2%
+  /// 
+  /// Array
+  /// </summary>
+  public class Solution
+  {
+    public int MaxAscendingSum(int[] numbers)
+    {
+      var result = 0;
+      var currentSum = 0;
+      var previous = numbers[0];
+      for (var i = 0; i < numbers.Length; i++)
+      {
+        if (numbers[i] > previous)
+        {
+          currentSum += numbers[i];
+        }
+        else
+        {
+          currentSum = numbers[i];
+        }
+
+        previous = numbers[i];
+        if (result < currentSum)
+          result = currentSum;
+      }
+
+      return result;
+    }
+  }
+}
