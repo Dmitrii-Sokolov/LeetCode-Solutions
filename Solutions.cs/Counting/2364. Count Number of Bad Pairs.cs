05c14182932ff13ca27660cc175685395d6a2bf9
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2025-02-09 07:39:41 +0100

    2364. Count Number of Bad Pairs

---
+++ Solutions.cs/Counting/2364. Count Number of Bad Pairs.cs
@@ -0,0 +1,33 @@
+﻿namespace Problem2364
+{
+
+  /// <summary>
+  /// 2364. Count Number of Bad Pairs
+  /// https://leetcode.com/problems/count-number-of-bad-pairs
+  /// 
+  /// Difficulty Medium
+  /// Acceptance 48.0%
+  /// 
+  /// Array
+  /// Hash Table
+  /// Math
+  /// Counting
+  /// </summary>
+  public class Solution
+  {
+    public long CountBadPairs(int[] numbers)
+    {
+      var offsets = new Dictionary<int, int>();
+      var goodPairs = 0L;
+      for (var i = 0; i < numbers.Length; i++)
+      {
+        var offset = numbers[i] - i;
+        var count = offsets.GetValueOrDefault(offset);
+        goodPairs += count;
+        offsets[offset] = count + 1;
+      }
+
+      return numbers.Length * (numbers.Length - 1L) / 2L - goodPairs;
+    }
+  }
+}
