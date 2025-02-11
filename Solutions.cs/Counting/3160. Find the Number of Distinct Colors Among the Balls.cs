76dcf5f5c9ba0dd9cf859da1044a62503d5efb6a
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2025-02-11 15:17:40 +0100

    3160. Find the Number of Distinct Colors Among the Balls

---
+++ Solutions.cs/Counting/3160. Find the Number of Distinct Colors Among the Balls.cs
@@ -0,0 +1,48 @@
+﻿namespace Problem3160
+{
+
+  /// <summary>
+  /// 3160. Find the Number of Distinct Colors Among the Balls
+  /// https://leetcode.com/problems/find-the-number-of-distinct-colors-among-the-balls
+  /// 
+  /// Difficulty Medium
+  /// Acceptance 54.3%
+  /// 
+  /// Array
+  /// Hash Table
+  /// Simulation
+  /// </summary>
+  public class Solution
+  {
+    public int[] QueryResults(int _, int[][] queries)
+    {
+      var result = new int[queries.Length];
+      var colors = new Dictionary<int, int>(queries.Length);
+      var colorsCount = new Dictionary<int, int>(queries.Length);
+      var currentColorsCount = 0;
+      for (var i = 0; i < queries.Length; i++)
+      {
+        var query = queries[i];
+        var ballIndex = query[0];
+        var newColor = query[1];
+
+        if (colors.TryGetValue(ballIndex, out var oldColor))
+        {
+          colorsCount[oldColor]--;
+          if (colorsCount[oldColor] == 0)
+            currentColorsCount--;
+        }
+
+        colorsCount[newColor] = colorsCount.GetValueOrDefault(newColor) + 1;
+        if (colorsCount[newColor] == 1)
+          currentColorsCount++;
+
+        colors[ballIndex] = newColor;
+
+        result[i] = currentColorsCount;
+      }
+
+      return result;
+    }
+  }
+}
