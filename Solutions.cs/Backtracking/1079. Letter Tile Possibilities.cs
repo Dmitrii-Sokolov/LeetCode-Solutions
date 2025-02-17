9381b466dc1c6e198b6dec719a3f700343a17728
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2025-02-17 07:19:00 +0100

    1079. Letter Tile Possibilities

---
+++ Solutions.cs/Backtracking/1079. Letter Tile Possibilities.cs
@@ -0,0 +1,44 @@
+﻿namespace Problem1079
+{
+
+  /// <summary>
+  /// 1079. Letter Tile Possibilities
+  /// https://leetcode.com/problems/letter-tile-possibilities
+  /// 
+  /// Difficulty Medium
+  /// Acceptance 78.9%
+  /// 
+  /// Hash Table
+  /// String
+  /// Backtracking
+  /// Counting
+  /// </summary>
+  public class Solution
+  {
+    public int NumTilePossibilities(string tilesRaw)
+    {
+      var counts = new Dictionary<char, int>();
+      foreach (var tile in tilesRaw)
+        counts[tile] = counts.GetValueOrDefault(tile) + 1;
+
+      var tiles = counts.Values.ToArray();
+      return Proceed(tiles) - 1;
+    }
+
+    private static int Proceed(int[] tiles)
+    {
+      var result = 1;
+      for (var i = 0; i < tiles.Length; i++)
+      {
+        if (tiles[i] > 0)
+        {
+          tiles[i]--;
+          result += Proceed(tiles);
+          tiles[i]++;
+        }
+      }
+
+      return result;
+    }
+  }
+}
