d024291f6367d34b1005bca2a82d79f5d39ddd0d
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2025-02-21 07:58:05 +0100

    1261. Find Elements in a Contaminated Binary Tree

---
+++ Solutions.cs/Design/1261. Find Elements in a Contaminated Binary Tree.cs
@@ -0,0 +1,40 @@
+﻿namespace Problem1261
+{
+
+  /// <summary>
+  /// 1261. Find Elements in a Contaminated Binary Tree
+  /// https://leetcode.com/problems/find-elements-in-a-contaminated-binary-tree
+  /// 
+  /// Difficulty Medium
+  /// Acceptance 81.1%
+  /// 
+  /// Hash Table
+  /// Tree
+  /// Depth-First Search
+  /// Breadth-First Search
+  /// Design
+  /// Binary Tree
+  /// </summary>
+  public class FindElements
+  {
+    private TreeNode Root;
+
+    public FindElements(TreeNode root) => Root = root;
+
+    public bool Find(int target)
+    {
+      target++;
+      var depth = int.Log2(target);
+      var index = target - (1 << depth);
+      var node = Root;
+      for (var level = depth - 1; level >= 0 && node != null; level--)
+      {
+        var mask = 1 << level;
+        node = (index & mask) > 0 ? node.right : node.left;
+        index &= ~mask;
+      }
+
+      return node != null;
+    }
+  }
+}
