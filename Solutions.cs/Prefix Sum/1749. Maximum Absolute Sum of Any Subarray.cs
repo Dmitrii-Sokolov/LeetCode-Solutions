20a55de1357d696f35d94ac9599fbdae8dd83e87
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2025-02-26 08:37:21 +0100

    1749. Maximum Absolute Sum of Any Subarray

---
+++ Solutions.cs/Prefix Sum/1749. Maximum Absolute Sum of Any Subarray.cs
@@ -0,0 +1,40 @@
+﻿namespace Problem1749
+{
+
+  /// <summary>
+  /// 1749. Maximum Absolute Sum of Any Subarray
+  /// https://leetcode.com/problems/maximum-absolute-sum-of-any-subarray
+  /// 
+  /// Difficulty Medium
+  /// Acceptance 66.8%
+  /// 
+  /// Array
+  /// Dynamic Programming
+  /// </summary>
+  public class Solution
+  {
+    public int MaxAbsoluteSum(int[] numbers)
+    {
+      var result = 0;
+      var prefix = 0;
+      var max = 0;
+      var min = 0;
+      foreach (var number in numbers)
+      {
+        if (min > prefix)
+          min = prefix;
+
+        if (max < prefix)
+          max = prefix;
+
+        prefix += number;
+
+        var candidate = prefix < 0 ? max - prefix : prefix - min;
+        if (result < candidate)
+          result = candidate;
+      }
+
+      return result;
+    }
+  }
+}
