a534e0ab336bc0f348efe5f7e56573c310b4d8df
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2025-03-01 10:10:07 +0100

    2460. Apply Operations to an Array

---
+++ Solutions.cs/Two Pointers/2460. Apply Operations to an Array.cs
@@ -0,0 +1,43 @@
+﻿namespace Problem2460
+{
+
+  /// <summary>
+  /// 2460. Apply Operations to an Array
+  /// https://leetcode.com/problems/apply-operations-to-an-array
+  /// 
+  /// Difficulty Easy
+  /// Acceptance 71.6%
+  /// 
+  /// Array
+  /// Two Pointers
+  /// Simulation
+  /// </summary>
+  public class Solution
+  {
+    public int[] ApplyOperations(int[] numbers)
+    {
+      var writePosition = 0;
+      for (var i = 0; i < numbers.Length - 1; i++)
+      {
+        if (numbers[i] > 0)
+        {
+          if (numbers[i] == numbers[i + 1])
+          {
+            numbers[writePosition++] = 2 * numbers[i];
+            numbers[i + 1] = 0;
+          }
+          else
+          {
+            numbers[writePosition++] = numbers[i];
+          }
+        }
+      }
+
+      numbers[writePosition++] = numbers[^1];
+      while (writePosition < numbers.Length)
+        numbers[writePosition++] = 0;
+
+      return numbers;
+    }
+  }
+}
