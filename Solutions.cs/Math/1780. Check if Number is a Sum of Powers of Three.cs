653ca398cf0ec0545d1e9d71beb7d32ca38f8624
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2025-03-04 06:36:35 +0100

    1780. Check if Number is a Sum of Powers of Three

---
+++ Solutions.cs/Math/1780. Check if Number is a Sum of Powers of Three.cs
@@ -0,0 +1,28 @@
+﻿namespace Problem1780
+{
+
+  /// <summary>
+  /// 1780. Check if Number is a Sum of Powers of Three
+  /// https://leetcode.com/problems/check-if-number-is-a-sum-of-powers-of-three
+  /// 
+  /// Difficulty Medium
+  /// Acceptance 73.9%
+  /// 
+  /// Math
+  /// </summary>
+  public class Solution
+  {
+    public bool CheckPowersOfThree(int n)
+    {
+      while (n > 0)
+      {
+        if (n % 3 == 2)
+          return false;
+
+        n /= 3;
+      }
+
+      return true;
+    }
+  }
+}
