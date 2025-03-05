19868484a7351bcf306dfd85d1c310296d52381d
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2025-03-05 06:25:34 +0100

    2579. Count Total Number of Colored Cells

---
+++ Solutions.cs/Math/2579. Count Total Number of Colored Cells.cs
@@ -0,0 +1,17 @@
+﻿namespace Problem2579
+{
+
+  /// <summary>
+  /// 2579. Count Total Number of Colored Cells
+  /// https://leetcode.com/problems/count-total-number-of-colored-cells
+  /// 
+  /// Difficulty Medium
+  /// Acceptance 64.3%
+  /// 
+  /// Math
+  /// </summary>
+  public class Solution
+  {
+    public long ColoredCells(long n) => n * (n - 1) * 2 + 1;
+  }
+}
