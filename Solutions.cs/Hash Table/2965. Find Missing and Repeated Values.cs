90668ea2740b9afc0c5a5a4e11933785b5874b1c
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2025-03-06 08:08:27 +0100

    2965. Find Missing and Repeated Values

---
+++ Solutions.cs/Hash Table/2965. Find Missing and Repeated Values.cs
@@ -0,0 +1,41 @@
+﻿namespace Problem2965
+{
+
+  /// <summary>
+  /// 2965. Find Missing and Repeated Values
+  /// https://leetcode.com/problems/find-missing-and-repeated-values
+  /// 
+  /// Difficulty Easy
+  /// Acceptance 81.0%
+  /// 
+  /// Array
+  /// Hash Table
+  /// Math
+  /// Matrix
+  /// </summary>
+  public class Solution
+  {
+    public int[] FindMissingAndRepeatedValues(int[][] grid)
+    {
+      var n = grid.Length;
+      var count = n * n;
+      var sum = (1 + count) * count / 2;
+      Span<bool> presented = stackalloc bool[count];
+      var result = new int[2];
+      foreach (var row in grid)
+      {
+        foreach (var number in row)
+        {
+          if (presented[number - 1])
+            result[0] = number;
+
+          presented[number - 1] = true;
+          sum -= number;
+        }
+      }
+
+      result[1] = result[0] + sum;
+      return result;
+    }
+  }
+}
