1e69f4505fe72086057cb1901f975dfa7825e172
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2025-03-17 06:52:12 +0100

    2206. Divide Array Into Equal Pairs

---
+++ Solutions.cs/Counting/2206. Divide Array Into Equal Pairs.cs
@@ -0,0 +1,31 @@
+﻿namespace Problem2206
+{
+
+  /// <summary>
+  /// 2206. Divide Array Into Equal Pairs
+  /// https://leetcode.com/problems/divide-array-into-equal-pairs
+  /// 
+  /// Difficulty Easy
+  /// Acceptance 76.7%
+  /// 
+  /// Array
+  /// Hash Table
+  /// Bit Manipulation
+  /// Counting
+  /// </summary>
+  public class Solution
+  {
+    public bool DivideArray(int[] numbers)
+    {
+      Span<bool> counts = stackalloc bool[500];
+      var count = 0;
+      foreach (var number in numbers)
+      {
+        counts[number - 1] = !counts[number - 1];
+        count += counts[number - 1] ? 1 : -1;
+      }
+
+      return count == 0;
+    }
+  }
+}
