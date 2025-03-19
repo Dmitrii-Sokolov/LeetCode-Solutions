21b2086e0cdf4b8a12014df36545ef8131e22756
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2025-03-19 06:47:23 +0100

    3191. Minimum Operations to Make Binary Array Elements Equal to One I

---
+++ Solutions.cs/Greedy/3191. Minimum Operations to Make Binary Array Elements Equal to One I.cs
@@ -0,0 +1,35 @@
+﻿namespace Problem3191
+{
+
+  /// <summary>
+  /// 3191. Minimum Operations to Make Binary Array Elements Equal to One I
+  /// https://leetcode.com/problems/minimum-operations-to-make-binary-array-elements-equal-to-one-i
+  /// 
+  /// Difficulty Medium
+  /// Acceptance 75.9%
+  /// 
+  /// Array
+  /// Bit Manipulation
+  /// Queue
+  /// Sliding Window
+  /// Prefix Sum
+  /// </summary>
+  public class Solution
+  {
+    public int MinOperations(int[] numbers)
+    {
+      var result = 0;
+      for (var i = 0; i < numbers.Length - 2; i++)
+      {
+        if (numbers[i] == 0)
+        {
+          numbers[i + 1] = 1 - numbers[i + 1];
+          numbers[i + 2] = 1 - numbers[i + 2];
+          result++;
+        }
+      }
+
+      return numbers[^1] == 1 && numbers[^2] == 1 ? result : -1;
+    }
+  }
+}
