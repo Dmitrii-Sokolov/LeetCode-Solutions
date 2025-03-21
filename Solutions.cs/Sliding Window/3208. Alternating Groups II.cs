e84c7b310adaf200f3bf74ce7cc0cb3ab9676abd
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2025-03-21 12:04:42 +0100

    3208. Alternating Groups II

---
+++ Solutions.cs/Sliding Window/3208. Alternating Groups II.cs
@@ -0,0 +1,33 @@
+﻿namespace Problem3208
+{
+
+  /// <summary>
+  /// 3208. Alternating Groups II
+  /// https://leetcode.com/problems/alternating-groups-ii
+  /// 
+  /// Difficulty Medium
+  /// Acceptance 49.5%
+  /// 
+  /// Array
+  /// Sliding Window
+  /// </summary>
+  public class Solution
+  {
+    public int NumberOfAlternatingGroups(int[] colors, int k)
+    {
+      var n = colors.Length;
+      var result = 0;
+      var start = 0;
+      for (var i = 0; i < n + k - 2; i++)
+      {
+        if (colors[i % n] == colors[(i + 1) % n])
+          start = i + 1;
+
+        if (i - start + 2 >= k)
+          result++;
+      }
+
+      return result;
+    }
+  }
+}
